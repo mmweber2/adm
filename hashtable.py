dfb8e3bc5072e66d136f77d9c8dd1e5a3cc94817
Mary Ellen Weber <mmweber2@gmail.com>
2016-03-02 13:55:34 -0600

    Added delete function, and made None or "" valid keys.

--- hashtable.py
+++ hashtable.py
@@ -1,22 +1,18 @@
 from collections import namedtuple
 
-# Empty object for use when deleting an object from the hash table.
-class Placeholder(object):
-  pass
 # Instead of Placeholder, let's try using "".
 
 Item = namedtuple('Item', ['key', 'value'])
 
 class Table(object):
-  # Total number of items currently in the hash table.
-  items = 0
-  # Non-empty spaces, including placeholders, in the hash table.
-  spaces_filled = 0
-  array = None
   INIT_SIZE = 10
 
   def __init__(self):
     self.array = [None for _ in xrange(self.INIT_SIZE)]
+    # Total number of items currently in the hash table.
+    self.items = 0
+    # Non-empty spaces, including placeholders, in the hash table.
+    self.spaces_filled = 0
 
   # If the key hashes to an available value, stores it there.
   # If the key hashes to a previously used value, stores it in the next
@@ -32,7 +28,7 @@ class Table(object):
       return
     # Spot is already full, with dummy or real value
     if self.array[hashval] is not None:
-      hashval = self._open_address(hashval + 1, self.array)
+      hashval = Table._open_address(hashval + 1, self.array)
     # Once a spot is found, the item is going in the bucket and counts increment
     self.array[hashval] = Item(key, value)
     self.spaces_filled += 1
@@ -40,31 +36,19 @@ class Table(object):
     if self.spaces_filled > len(self.array) / 2:
       self.resize()
 
-  def lookup(self, key, hashval=None):
+  def lookup(self, key):
+    index = self._get_lookup_index(key)
+    if index is None:
+      raise KeyError("Key \"{}\" not found.".format(key))
+    return self.array[index].value
+
+  # Find the index of a key in array, if it exists.
+  # Returns None if it doesn't exist.
+  def _get_lookup_index(self, key, hashval=None):
     if hashval is None:
       hashval = hash(key) % len(self.array)
     while hashval < len(self.array):
-      current_slot = self.array[hashval]
-      if current_slot is None:
-        raise KeyError("Key \"{}\" not found.".format(key))
-      if type(current_slot) is Item:
-        if current_slot.key == key:
-          return current_slot.value
-      # Slot is a Placeholder or has another key in it
-        hashval += 1
-    # Cycle back and check beginning of array.
-    return self.lookup(key, 0)
-
-  # Find the index of a key in array, if it exists.
-  # Returns None if it doesn't exist, or KeyError if the key is invalid.
-  @staticmethod
-  def _get_lookup_index(key, array, hashval=None):
-    if key is None or key is "":
-      raise KeyError("Key {} is invalid.")
-    if hashval is None:
-      hashval = hash(key) % len(array)
-    while hashval < len(array):
-      current_bucket = array[hashval]
+      current_bucket = self.array[hashval]
       # Key is not in array.
       if current_bucket is None:
         return None
@@ -74,17 +58,22 @@ class Table(object):
           return hashval
       hashval += 1
     # Cycle back and check beginning of array.
-    return Table._get_lookup_index(key, array, 0)
+    # This won't result in a full loop because we'll have to hit a None
+    #   before looping through the array, since it is no more than half full.
+    return self._get_lookup_index(key, 0)
 
   def delete(self, key):
-    pass
+    index = self._get_lookup_index(key)
+    # _get_lookup_index will find if the key is already deleted or
+    #  was never added, so we don't have to check for that here.
+    self.array[index] = ""
+    self.items -= 1
 
   def _update_key(self, hashval, key, val):
-    index = Table._get_lookup_index(key, self.array)
+    index = self._get_lookup_index(key)
     # Key is not already in dictionary.
     if index is None:
       return False
-    # Key's index was found. 
     # Key is already in the table, so overwrite it without changing any sizes.
     self.array[hashval] = self.array[hashval]._replace(value=val)
     return True
@@ -96,14 +85,16 @@ class Table(object):
       if type(bucket) is Item:
         hashval = hash(bucket.key) % new_size
         if new_array[hashval] is not None:
-          hashval = self.open_address(hashval, new_array)
+          hashval = Table.open_address(hashval, new_array)
         # Don't increment the size, because that was already done for this item
         new_array[hashval] = bucket
-      if type(bucket) is Placeholder:
+      # Placeholder spot
+      if bucket == "":
         self.spaces_filled -= 1
 
   # Bucket already has a dummy or valid key/value pair, so find a new bucket.
-  def _open_address(self, hashval, array):
+  @staticmethod
+  def _open_address(hashval, array):
     while hashval < len(array):
       # New hash value found.
       if array[hashval] is None:
@@ -112,7 +103,7 @@ class Table(object):
       # Reached end of array before finding an open space; start at the front.
       # It may seem that this would only happen if the hash is very full,
       #   but it's possible for an item to hash near the end of the array.
-    return self._open_address(0, array)
+    return Table._open_address(0, array)
 
 # Hash key.
 # Check if bucket is empty: if yes, add it.
