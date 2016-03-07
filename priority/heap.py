d259b1202172c1bd70c77a5c15f240056d24cb5d
Mary Ellen Weber <mmweber2@gmail.com>
2016-03-06 19:39:03 -0600

    Created heap.py and its constructor.

---
+++ priority/heap.py
@@ -0,0 +1,13 @@
+# A data structure of ordered keys with quick access to smallest or largest
+# key in the set.
+class Heap(object):
+
+  def __init__(self):
+    self.size = 0
+    # Start with 0 as index 0 for easy integer division.
+    self.heap_list = [0]
+
+  # Insert object with value value.
+  def insert(self, value):
+    pass
+
