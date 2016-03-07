6d8353012e7d0ba25f48512ec0ed5171d19c156a
Mary Ellen Weber <mmweber2@gmail.com>
2016-03-07 13:20:47 -0600

    Implemented insert.

--- priority/heap.py
+++ priority/heap.py
@@ -2,12 +2,21 @@
 # key in the set.
 class Heap(object):
 
-  def __init__(self):
-    self.size = 0
-    # Start with 0 as index 0 for easy integer division.
-    self.heap_list = [0]
+    def __init__(self):
+        self.size = 0
+        # Start with 0 as index 0 for easy integer division.
+        self.heap_list = [0]
 
-  # Insert object with value value.
-  def insert(self, value):
-    pass
+    # Insert object with value value.
+    def insert(self, value):
+        self.heap_list.append(value)
+        self.size += 1
+        self._heapify(self.size)
+
+    def _heapify(self, index):
+        while index / 2 > 0:
+            if self.heap_list[index] < self.heap_list[index / 2]:
+                (self.heap_list[index], self.heap_list[index / 2]) = (
+                    self.heap_list[index / 2], self.heap_list[index])
+            index /= 2
 
