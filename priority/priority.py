dfcabd60dbad1487b47efaa255c53ee9e694c33e
Mary Ellen Weber <mmweber2@gmail.com>
2016-03-14 12:14:28 -0500

    Create a basic (non-working) Queue class.

--- priority/priority.py
+++ priority/priority.py
@@ -1,13 +1,24 @@
 import heap
 
 class Queue(object):
+  """An unbounded priority queue based on a heap."""
   self.heap = Heap()
 
-  # TODO: What should the queue pop if it's empty?
+  # TODO: Implement adding key/item pairs as tuples.
+
   def pop(self):
       """Returns the smallest item in the queue.
+
+      Raises an IndexError if the queue is empty.
       """
       return self.heap.pop()
+
+  def peek(self):
+      """Returns a copy of the smallest item in the queue.
+
+      Raises an IndexError if the queue is empty.
+      """
+      return self.heap.peek()
 
   def push(self, value):
       """Adds an item to the queue."""
