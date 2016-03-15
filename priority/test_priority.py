ad7710c5fcfeb57ce10de10b1afd5f4d4b4ded3c
Mary Ellen Weber <mmweber2@gmail.com>
2016-03-15 09:05:44 -0500

    Fill in tests for Queue.

---
+++ priority/test_priority.py
@@ -0,0 +1,26 @@
+from priority import Queue
+from nose.tools import assert_raises
+
+def test_push_and_peek():
+  a = Queue()
+  a.push(1, "test")
+  assert a.peek() == "test"
+  a.push(3, "test again")
+  assert a.peek() == "test again"
+
+def test_pop():
+  a = Queue()
+  a.push("string", "methods")
+  a.push("are", "useful")
+  assert a.pop() == "useful"
+  assert a.pop() == "methods"
+
+def test_peek_items_removed():
+  a = Queue()
+  a.push(3, "remove")
+  a.pop()
+  assert_raises(IndexError, a.peek)
+
+def test_peek_ever_empty():
+  a = Queue
+  assert_raises(IndexError, a.peek)
