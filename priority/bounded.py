ffd968e7961f85e3f365ab77e02c0d092998be98
Mary Ellen Weber <mmweber2@gmail.com>
2016-03-17 12:22:03 -0500

    Correct some tests that had the wrong format for assert.raises.

--- priority/bounded.py
+++ priority/bounded.py
@@ -12,14 +12,34 @@ class BoundedNode(object):
 class BoundedQueue(object):
     """A bounded-height priority queue."""
 
-     def __init__(self, limit):
-         """Creates a new BoundedQueue for keys in the range 1
-         to limit, where limit is an integer >= 2.
+    def __init__(self, limit):
+        """Creates a new BoundedQueue for keys in the range 1
+        to limit, where limit is an integer >= 2.
 
-         Raises a ValueError if limit is not an integer, or
-         an IndexError if limit is less than 2.
+        Raises a ValueError if limit is not an integer, or a IndexError
+        if limit is less than 2.
+        """
+        if limit < 2:
+          raise IndexError("limit must be greater than 1.")
+        limit = int(limit)
+        # Allow 1-indexing of the array.
+        limit += 1
+        self.array = [None] * limit
+        self.top = limit
+
+    def insert(self, key, value):
+        """Insert a key, value pair into the queue.
+
+         Raises a TypeError if key is not an integer, or a IndexError
+         if key is not greater than 0 and less than or equal to limit,
+         as defined when the queue was created.
          """
-         if limit < 2:
-           raise IndexError("limit must be greater than 1.")
-         self.array = [None] * limit
-         # Todo: Test key for validity (int >1 and < limit)
+        # Todo: Test key for validity (int >1 and < limit)
+        # When an item is removed, top is incremented until it reaches a
+        # non-empty list.
+
+        # Make a new Node for this key.
+        if self.array[key] == None:
+            self.array[key] = BoundedNode(key, value)
+            self.top = min(self.top, key)
+        #TODO: If there's already a node there, child it.
--- priority/test_bounded.py
+++ priority/test_bounded.py
@@ -6,21 +6,38 @@ def test_create_valid():
     q = BoundedQueue(2)
 
 def test_create_one():
-    assert_raises(IndexError, BoundedQueue(1))
+    assert_raises(IndexError, BoundedQueue, 1)
 
 def test_create_string():
-    assert_raises(ValueError, BoundedQueue("test"))
+    assert_raises(ValueError, BoundedQueue, "test")
 
 def test_create_float():
     q = BoundedQueue(5.0)
 
-def test_insert():
-    # TODO: Try key, value
+def test_insert_first():
+    q = BoundedQueue(2)
+    q.insert(1, "test")
+    node = q.array[1]
+    assert q.array == [None, node, None]
+    assert node.key == 1
+    assert node.value == "test"
 
-  """When the set of keys is {1, 2, ..., C}, and only insert, 
-  find-min and extract-min are needed, a bounded height priority
-  queue can be constructed as an array of C linked lists plus a pointer top,
-  initially C. Inserting an item with key k appends the item to the k'th,
-  and updates top ← min(top, k), both in constant time. Extract-min deletes and
-  returns one item from the list with index top, then increments top if needed 
-  until it again points to a non-empty list; this takes O(C) time in the worst case""".
+def test_insert_invalid_int():
+    pass
+
+def test_insert_not_int():
+    pass
+
+def test_insert_None():
+    pass
+
+def test_find_min():
+    pass
+
+def test_find_min_empty():
+    pass
+
+def test_extract_min():
+    pass
+
+    # TODO: Try key, value
