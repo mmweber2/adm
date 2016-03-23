029d5990b717ccb312de27216e95be131d794870
Mary Ellen Weber <mmweber2@gmail.com>
2016-03-23 10:22:27 -0500

    Small changes per code review.
    Remove size() and test it through other functions.
    Change various tests to use external details when possible (such as
         checking the values with peek() or pop() instead of the internal
        _heap_list).

--- priority/test_heap.py
+++ priority/test_heap.py
@@ -6,15 +6,11 @@ def test_new():
     assert a.size() == 0
     assert a.heap_list == [0]
 
-def test_size():
-    a = Heap()
-    assert a.size() == 0
-
 def test_push_first():
     a = Heap()
     a.push(2)
     assert a.size() == 1
-    assert a.heap_list == [0, 2]
+    assert a.peek() == 2
 
 def test_peek():
     """Other functions test the ordering of the elements elsewhere,
@@ -23,20 +19,25 @@ def test_peek():
     a = Heap()
     a.push(3)
     assert a.peek() == 3
+    assert a.size() == 1
+    # Element has not been removed
+    assert a.peek() == 3
 
 def test_push_increasing():
     a = Heap()
     a.push(2)
     a.push(4)
     assert a.size() == 2
-    assert a.heap_list == [0, 2, 4]
+    assert a.pop() == 2
+    assert a.pop() == 4
 
 def test_push_decreasing():
     a = Heap()
     a.push(4)
     a.push(2)
     assert a.size() == 2
-    assert a.heap_list == [0, 2, 4]
+    assert a.pop() == 2
+    assert a.pop() == 4
 
 def test_push_out_of_order():
     """Prove that the heap_list isn't just sorting the values."""
@@ -54,7 +55,10 @@ def test_push_multiple_heapify():
     a.push(3)
     a.push(1)
     assert a.size() == 4
-    assert a.heap_list == [0, 1, 2, 3, 4]
+    assert a.pop() == 1
+    assert a.pop() == 2
+    assert a.pop() == 3
+    assert a.pop() == 4
 
 def test_push_duplicates():
     a = Heap()
@@ -62,19 +66,24 @@ def test_push_duplicates():
     a.push(7)
     a.push(5)
     assert a.size() == 3
-    assert a.heap_list == [0, 5, 7, 5]
+    assert a.pop() == 5
+    assert a.pop() == 5
+    assert a.pop() == 7
 
 def test_push_zero():
     a = Heap()
     a.push(0)
     assert a.size() == 1
-    assert a.heap_list == [0, 0]
+    assert a.pop() == 0
 
 def test_push_negative():
+    """Prove that the 0 at index 0 won't get in the way of
+       keys that compare as less than 0.
+    """
     a = Heap()
     a.push(-1)
     assert a.size() == 1
-    assert a.heap_list == [0, -1]
+    assert a.pop() == -1
 
 def test_pop_empty():
     a = Heap()
@@ -85,7 +94,6 @@ def test_pop_one():
     a.push(1)
     assert a.pop() == 1
     assert a.size() == 0
-    assert a.heap_list == [0]
     assert_raises(IndexError, a.pop)
 
 def test_pop_three():
@@ -95,7 +103,8 @@ def test_pop_three():
     a.push(2)
     assert a.pop() == -1
     assert a.size() == 2
-    assert a.heap_list == [0, 1, 2]
+    assert a.pop() == 1
+    assert a.pop() == 2
 
 def test_pop_three_levels():
     a = Heap()
