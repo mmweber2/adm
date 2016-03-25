04edc9870890a043db6fe2a1ea5c43c77ab64e92
Mary Ellen Weber <mmweber2@gmail.com>
2016-03-25 18:35:07 -0500

    Partial outline of SuffixTree class.

---
+++ suffix/suffix_tree.py
@@ -0,0 +1,11 @@
+class SuffixArray(object):
+    """An array that keeps track of suffixes."""
+    def __init__(self):
+        """Create a new, empty SuffixArray."""
+        pass
+
+    def add_string(self, s):
+        """Add string s to the SuffixArray."""
+        pass
+    # TODO: Add all strings at once?
+    # Track Longest Common Prefix?
