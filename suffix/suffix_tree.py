108124a37b29cefae7fb056793ddd09042f1d4cf
Mary Ellen Weber <mmweber2@gmail.com>
2016-03-26 07:55:51 -0500

    Add is_substring and several tests.

--- suffix/suffix_tree.py
+++ suffix/suffix_tree.py
@@ -1,3 +1,5 @@
+from bisect import bisect, bisect_left
+
 class SuffixArray(object):
     """An array that keeps track of suffixes."""
     def __init__(self, s):
@@ -11,6 +13,26 @@ class SuffixArray(object):
         """
         s = str(s)
         self.array = sorted([s[i:] for i in xrange(len(s))])
+
+    # TODO: Count substrings too?
+    def is_substring(self, sub):
+        """Returns True iff sub is a substring of the string in
+        this SuffixArray.
+
+        An empty string is considered a substring of any string.
+        """
+        if sub == "":
+            return True
+        # Find where the substring would go if we added it
+        start = bisect_left(self.array, sub)
+        # Increment the last character of sub as little as possible
+        # in order to find the first following non-prefix word
+        increment_last_char = sub[:-1] + chr(ord(sub[-1]) + 1)
+        end = bisect_left(self.array, increment_last_char)
+        # If start < end, there is at least one suffix in s that
+        # has sub as a prefix, which means that sub is a substring.
+        return start < end
+
 
     # TODO: Add more strings?
     # Track Longest Common Prefix?
--- suffix/test_suffix.py
+++ suffix/test_suffix.py
@@ -3,6 +3,37 @@ from nose.tools import assert_raises
 
 # Black box
 
+def test_is_substring_middle():
+    a = SuffixArray("banana")
+    assert a.is_substring("nan")
+
+def test_is_substring_front():
+    a = SuffixArray("string tests")
+    assert a.is_substring("strin")
+
+def test_is_substring_end():
+    a = SuffixArray("chairs")
+    assert a.is_substring("airs")
+
+def test_is_substring_false():
+    a = SuffixArray("test")
+    assert not a.is_substring("tired")
+
+def test_is_substring_false_incremented():
+    a = SuffixArray("test")
+    assert not a.is_substring("tet")
+
+def test_is_substring_false_longer():
+    a = SuffixArray("test")
+    assert not a.is_substring("testing")
+
+def test_is_substring_empty():
+    """Since this function relies on the last character, try an
+    empty string.
+    """
+    a = SuffixArray("test")
+    assert a.is_substring("")
+
 # White box
 
 def test_create_string():
