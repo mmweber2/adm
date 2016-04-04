5ad7fee021d287a2de5641a1ce31cd0f318d1122
Mary Ellen Weber <mmweber2@gmail.com>
2016-04-04 10:49:11 +0900

    Update tests per code review.
    Remove one float test (testing a non-integer float).
    Modify test_create_float so that it fails when creating a queue of size 5.0.
    Remove test_find_min_priority_chain because it relies on the order of elements.
    Modify test_insert_collision to use a set and not test specific order
    Correct a find/replace error of ‘prioritys’ to ‘priorities’ on lines 72 and 96.
    Modify test_extract_min_same_priority to use a set and not test specific order.
    Correct q.item_count to len(q) on line 87.
    Add test_insert_3_with_1_collision, where the non-colliding element is inserted
      second.
    Add 5 tests for len()

--- priority/test_bounded.py
+++ priority/test_bounded.py
@@ -11,28 +11,23 @@ def test_create_one():
 def test_create_string():
     assert_raises(ValueError, BoundedQueue, "test")
 
-def test_create_float_whole():
-    q = BoundedQueue(5.0)
-    assert q.size() == 5
-
-def test_create_float_trailing():
-    q = BoundedQueue(2.5)
-    assert q.size() == 2
+def test_create_float():
+    assert_raises(ValueError, BoundedQueue, "5.0")
 
 def test_insert_first():
     q = BoundedQueue(2)
     q.insert(1, "test")
     node = q.array[1]
     assert q.array == [None, node, None]
-    assert node.key == 1
-    assert node.value == "test"
+    assert node.priority == 1
+    assert node.data == "test"
 
-def test_insert_maximum_key():
+def test_insert_maximum_priority():
     q = BoundedQueue(2)
     q.insert(2, "max")
     node = q.array[2]
-    assert node.key == 2
-    assert node.value == "max"
+    assert node.priority == 2
+    assert node.data == "max"
 
 def test_insert_invalid_int():
     q = BoundedQueue(2)
@@ -54,40 +49,31 @@ def test_insert_collision():
     q = BoundedQueue(3)
     q.insert(1, "test")
     q.insert(1, "again")
-    node = q.array[1]
-    assert node.key == 1
-    assert node.value == "test"
-    child = node.child
-    assert child.key == 1
-    assert child.value == "again"
+    # Don't test on the order, since implementation may change.
+    nodes = set((q.array[1].data, q.array[1].next_item.data))
+    assert "test" in nodes
+    assert "again" in nodes
 
 def test_insert_not_collision():
     q = BoundedQueue(3)
     q.insert(1, "test")
     q.insert(2, "again")
     node = q.array[2]
-    assert node.key == 2
-    assert node.value == "again"
+    assert node.priority == 2
+    assert node.data == "again"
 
 def test_find_min_single_item():
     q = BoundedQueue(3)
     q.insert(1, "test")
-    value = q.find_min()
-    assert value == "test"
+    data = q.find_min()
+    assert data == "test"
 
-def test_find_min_multiple_keys():
+def test_find_min_multiple_priorities():
     q = BoundedQueue(3)
     q.insert(1, "test")
     q.insert(2, "again")
-    value = q.find_min()
-    assert value == "test"
-
-def test_find_min_key_chain():
-    """Find_min should not be sorting the values within a key."""
-    q = BoundedQueue(3)
-    q.insert(1, "test")
-    q.insert(1, "again")
-    assert q.find_min() == "test"
+    data = q.find_min()
+    assert data == "test"
 
 def test_find_min_empty():
     q = BoundedQueue(2)
@@ -97,21 +83,32 @@ def test_extract_min_standard():
     q = BoundedQueue(2)
     q.insert(2, "test")
     assert q.extract_min() == "test"
-    assert q.item_count == 0
+    assert len(q) == 0
 
-def test_extract_min_same_key():
+def test_extract_min_same_priority():
     q = BoundedQueue(2)
     q.insert(2, "test")
     q.insert(2, "test two")
-    assert q.extract_min() == "test"
-    assert q.extract_min() == "test two"
+    nodes = set((q.extract_min(), q.extract_min()))
+    assert "test" in nodes
+    assert "test two" in nodes
 
-def test_extract_min_different_keys():
+def test_extract_min_different_priorities():
     q = BoundedQueue(4)
     q.insert(3, "test")
     q.insert(1, "testing")
     assert q.extract_min() == "testing"
     assert q.extract_min() == "test"
+
+def test_insert_3_with_1_collision():
+    q = BoundedQueue(3)
+    q.insert(2, "test 1")
+    q.insert(1, "test 2")
+    q.insert(2, "test 3")
+    assert q.extract_min() == "test 2"
+    nodes = set((q.extract_min(), q.extract_min()))
+    assert "test 1" in nodes
+    assert "test 3" in nodes
 
 def test_extract_min_empty():
     q = BoundedQueue(2)
@@ -122,4 +119,32 @@ def test_extract_min_already_removed():
     q.insert(3, "test")
     q.extract_min()
     assert_raises(IndexError, q.extract_min)
+
+def test_len_empty():
+    q = BoundedQueue(4)
+    assert len(q) == 0
+
+def test_len_single():
+    q = BoundedQueue(3)
+    q.insert(1, "test")
+    assert len(q) == 1
+
+def test_len_removed_one():
+    q = BoundedQueue(3)
+    q.insert(2, "test")
+    q.extract_min()
+    assert len(q) == 0
+
+def test_len_collision():
+    q = BoundedQueue(2)
+    q.insert(1, "test")
+    q.insert(1, "test again")
+    assert len(q) == 2
+
+def test_len_collision_removed():
+    q = BoundedQueue(3)
+    q.insert(1, "test")
+    q.insert(1, "test again")
+    q.extract_min()
+    assert len(q) == 1
 
