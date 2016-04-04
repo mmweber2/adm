f63b020bacd861a6ef44d573c4582f83822608be
Mary Ellen Weber <mmweber2@gmail.com>
2016-04-04 13:35:43 +0900

    Add to longest_repeating().
    Make various comment corrections.
    Add _ to to self.array and self.lcp to mark that they shouldn’t be modified.
    Fix error in longest_repeating where max_pair was being initialized as a
      single list, not a list of tuples.
    Fix error in longest_repeating where strings were not being added to the
      return value if they matched until the end of the string.

--- suffix/suffix_tree.py
+++ suffix/suffix_tree.py
@@ -3,32 +3,43 @@ from bisect import bisect_left
 class SuffixArray(object):
     """An array that keeps track of suffixes."""
     def __init__(self, s):
-        """Create a new, sorted SuffixArray of string s.
+        """Creates a new, sorted SuffixArray of string s.
 
         Suffixes are sorted using sorted(), so they are case sensitive:
         "None" is less than "none".
 
-        If s is not a string, it will be converted to one with str().
+        Args:
+            s: The string from which to form the SuffixArray.
+               If s is not a string, it will be converted to one with
+               str().
         """
         s = str(s)
-        self.array = sorted([s[i:] for i in xrange(len(s))])
-        # Determine LCPs for each pair of (sorted) consecutive prefixes.
-        # First item will have no common prefix because it has no predecessor.
-        self.lcp = [0]
-        for i in xrange(1, len(self.array)):
-            prefix = SuffixArray._find_lcp(self.array[i], self.array[i-1])
-            self.lcp.append(prefix)
+        self._array = sorted([s[i:] for i in xrange(len(s))])
+        # Determine LCPs (longest common prefixes) for each pair of
+        # (sorted) consecutive prefixes.
+        # The first item will have no common prefix because it has no
+        # predecessor.
+        self._lcp = [0]
+        for i in xrange(1, len(self._array)):
+            prefix = SuffixArray._find_lcp(self._array[i], self._array[i-1])
+            self._lcp.append(prefix)
 
     @staticmethod
     def _find_lcp(s1, s2):
-        """Returns the length of the longest common prefix
-        of strings s1 and s2.
+        """Finds the length of the longest common prefix of s1 and s2.
 
-        Returns 0 if there are no elements in common.
-        Raises a TypeError if either or both elements are not strings.
+        Args:
+            s1, s2: The strings to evaluate.
+
+        Returns:
+            The length of the longest common prefix of s1 and 2, or
+            0 if there are no elements in common.
+
+        Raises:
+            TypeError: s1 or s2 is not a string.
         """
-        # Comparisons and/or iterations might work with other types,
-        # so check the types first.
+        # Comparisons work with other non-string types, so check
+        # the types first.
         if not (isinstance(s1, str) and isinstance(s2, str)):
             raise TypeError
         # Put the shorter string first for ease of iteration.
@@ -41,67 +52,86 @@ class SuffixArray(object):
             else: break
         return count
 
-    # The bisect version works, but it might be making more comparisons
-        # than are necessary.
-
-    # TODO: Count substrings too?
+    # TODO: Count how many of the substrings too?
     def is_substring(self, sub):
-        """Returns True iff sub is a substring of the string in
-        this SuffixArray.
+        """Checks for a substring in this string.
 
         An empty string is considered a substring of any string.
+
+        Args:
+            sub: The substring to check for in this SuffixArray.
+
+        Returns:
+            True iff sub is a substring of the string used to create
+            this SuffixArray.
         """
         if sub == "":
             return True
-        # Find where the substring would go if we added it
-        start = bisect_left(self.array, sub)
+        # Find where the substring would go if we were to add it
+        start = bisect_left(self._array, sub)
         # Increment the last character of sub as little as possible
         # in order to find the first following non-prefix word
+        # Example: 'cass' would be incremented to 'cast'
         increment_last_char = sub[:-1] + chr(ord(sub[-1]) + 1)
-        end = bisect_left(self.array, increment_last_char)
+        end = bisect_left(self._array, increment_last_char)
         # If start < end, there is at least one suffix in s that
         # has sub as a prefix, which means that sub is a substring.
         return start < end
 
 
+    # TODO: Number of occurrences of x in string?
+        # Find place in sort where suffixes start with x
+        # Count number of these
     # TODO
     def longest_repeating(self):
-        """Returns a list of all of the longest repeating substrings
-        in this SuffixArray.
+        """Find the longest repeating substring.
 
-        If there are multiple repeating substrings that are all the
-        maximum size, all are returned in a list. Otherwise, the list
-        consists of the single longest repeating substring.
+        Returns:
+            A list of all of the longest repeating substrings
+            in this SuffixArray.
 
-        Returns an empty list if the SuffixArray contains no repeating
-        substrings.
+            If there are multiple repeating substrings that are all the
+            same (maximum) size, they are all returned in a list.
+            Otherwise, the list consists of the single longest repeating
+            substring.
+            To compare characters, the default comparator is used and
+            whitespace and punctuation are not ignored.
+
+            Returns an empty list if the SuffixArray contains no repeating
+            substrings.
         """
         # List of tuples of all max pairs
         max_pair = []
         max_size = 0
-        # Get pair starting with the longest repeating string.
-        for i in xrange(1, len(self.lcp)):
-            pairs = self.lcp[i]
-            if pairs > max_size:
-                max_size = pairs
-                # Reset max_pair, since a new size was found
-                # We don't have to worry about i-1 at 0 because i's
-                # value will always be 0 at index 0.
-                max_pair = [self.array[i-1], self.array[i]]
-            elif pairs == max_size:
-                max_pair.append((self.array[i-1], self.array[i]))
+        # Get the pairs that start with the longest repeating string.
+        # Start at 1 because lcp[0] is always 0.
+        for i in xrange(1, len(self._lcp)):
+            prefix_size = self._lcp[i]
+            if prefix_size > max_size:
+                max_size = prefix_size
+                # Reset max_pair because a new size was found.
+                # lcp[i] reflects the size of the shared prefix
+                # between array[i] and array[i-1], so track both
+                # of those.
+                max_pair = [(self._array[i-1], self._array[i])]
+            elif prefix_size == max_size:
+                max_pair.append((self._array[i-1], self._array[i]))
         # Now that we have these pairs, figure out the longest repeating
+        # part of each of them.
         # List of all max-size repeating substrings (not tuples)
         all_max = []
         for pair in max_pair:
-            print "Max size is ", max_size
-            print "Max pair is ", max_pair
+            # TODO: Remove print statements
+            # Iterate through the shorter string
             shorter = pair[0]
             longer = pair[1]
             if len(pair[1]) < len(pair[0]):
                 shorter = pair[1]
                 longer = pair[0]
+            common_substring = ""
             for i in xrange(len(shorter)):
-                if shorter[i] != longer[i]:
-                all_max.append(shorter[:i])
+                if shorter[i] == longer[i]:
+                    common_substring += shorter[i]
+                else: break
+            all_max.append(common_substring)
         return all_max
