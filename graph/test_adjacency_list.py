384759c0c79016300fb31ba2a347debbc97ca2dc
Mary Ellen Weber <mmweber2@gmail.com>
2016-04-17 14:55:55 +0900

    Update another test to use size() instead of len().

--- graph/test_adjacency_list.py
+++ graph/test_adjacency_list.py
@@ -77,7 +77,7 @@ def test_extra_pipes():
 
 def test_duplicate_edges():
     a = AdjacencyList("duplicate_edges.txt")
-    assert len(a.vertices) == 2
+    assert a.size() == 2
     assert a.vertices["Sam"].edges["Sally"].weight == "15"
 
 def test_size():
