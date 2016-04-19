fa7d6618afdb76196c478d44c02328b09aa03cfa
Mary Ellen Weber <mmweber2@gmail.com>
2016-04-19 13:05:56 +0900

    Fix issues related to AdjacencyList structure.
    Correct lines referring to Edge objects; we don't need the Edges themselves,
        as those are primarily to track weights.
    Get rid of the vertex_name stack, as we can use processed instead and just
        accept the linear search time to see if a node is processed.

--- graph/topol.py
+++ graph/topol.py
@@ -4,26 +4,30 @@ def top_sort(graph):
     """Returns a topological sort of a graph.
 
     Orders the vertices in graph on a line such that all directed
-    edges go from left to right. Works only on directed acyclic graphs.
+    edges go from left to right.
 
     Args:
         graph: AdjacencyList graph to sort. Must be acyclic.
 
     Returns:
         A list of the vertices of the graph in topological order.
+
+    Raises:
+        ValueError: graph contains a cycle.
     """
     discovered = set()
     processed = []
+    verts = graph.vertices
     # How do we get a valid starting point?
-    for vertex in graph.vertices.iterkeys():
-        for edge in vertex.edges:
-            _process_edge(graph.vertices[edge])
-        if vertex not in processed:
-            processed.append(vertex)
+    for vertex in verts.itervalues():
+        for edge in vertex.edges.iterkeys():
+            _process_edge(verts, verts[edge], discovered, processed)
+        if vertex.name not in processed:
+            processed.append(vertex.name)
     return processed[::-1]
 
 
-def _process_edge(edges, edge, discovered, processed):
+def _process_edge(vertices, edge, discovered, processed):
     """Process an edge and its own edges."""
     if edge in discovered:
         if edge.name in processed:
@@ -32,7 +36,7 @@ def _process_edge(edges, edge, discovered, processed):
             raise ValueError("graph cannot contain a cycle")
     discovered.add(edge)
     for y in edge.edges.iterkeys():
-        _process_edge(edges[y])
+        _process_edge(vertices, vertices[y], discovered, processed)
     processed.append(edge.name)
 
 
