3dc142e430f466b68f6426de932e886d8061c35e
Mary Ellen Weber <mmweber2@gmail.com>
2016-04-22 16:06:23 +0900

    Create Edge and add_edge function.

--- graph/graph.py
+++ graph/graph.py
@@ -4,11 +4,11 @@ class Vertex(object):
     existing = set()
 
     def __init__(self, name, edges=None):
-        """Create a new Vertex.
+        """Creates a new Vertex.
 
         Args:
-            name: The name to be used to identify this Vertex.
-                Must be unique.
+            name: The string used to identify this Vertex.
+                Must be unique, but is not case sensitive.
             edges (optional): A list of edges from this Vertex. If no list is
                 given, a new one will be created.
 
@@ -16,7 +16,7 @@ class Vertex(object):
             ValueError: The name given for this Vertex is already in use.
             TypeError: edges is given, but is not a list.
         """
-        if name in existing:
+        if name in self.existing:
             raise ValueError("Name {} already exists".format(name))
         self.name = name
         self.existing.add(name)
@@ -26,3 +26,32 @@ class Vertex(object):
             if not isinstance(edges, list):
                 raise TypeError("edges attribute must be a list")
             self.edges = edges
+
+    def add_edge(self, edge):
+        """Adds a new Edge to this Vertex.
+
+        Multiedges are supported, so Edges with the same name may
+            be added multiple times.
+
+        Args:
+            edge: The Edge to add. May or may not have a weight.
+
+        Raises:
+            TypeError: edge is not an Edge object.
+        """
+        if not isinstance(edge, Edge):
+            raise TypeError("edge must be an Edge object")
+        self.edges.append(edge)
+
+class Edge(object):
+
+    def __init__(self, name, weight=None):
+        """Create a new Edge.
+
+        Args:
+            name: The name of the Vertex this Edge points to.
+            weight: The weight of this Edge. Defaults to None.
+        """
+        self.name = name
+        self.weight = weight
+
--- graph/test_graph.py
+++ graph/test_graph.py
@@ -1,4 +1,5 @@
 from graph import Vertex
+from graph import Edge
 from nose.tools import assert_raises
 
 def test_new_vertex():
@@ -7,8 +8,34 @@ def test_new_vertex():
     assert a.edges == []
 
 def test_duplicate_vertex_name():
-    a = Vertex("A")
-    assert_raises(ValueError, Vertex, "A")
+    a = Vertex("B")
+    assert_raises(ValueError, Vertex, "B")
 
 def test_vertex_invalid_edges():
-    assert_raises(TypeError, Vertex, "A", "B")
+    assert_raises(TypeError, Vertex, "C", "B")
+
+def test_edge_default():
+    a = Edge("X")
+    assert a.name == "X"
+    assert a.weight == None
+
+def test_add_edge_normal():
+    a = Vertex("Test")
+    b = Edge("Test 2")
+    a.add_edge(b)
+    assert len(a.edges) == 1
+    assert b in a.edges
+
+def test_add_edge_duplicate():
+    a = Vertex("AB")
+    b = Edge("ABC")
+    c = Edge("ABC")
+    a.add_edge(b)
+    a.add_edge(c)
+    assert len(a.edges) == 2
+    assert b in a.edges
+    assert c in a.edges
+
+def test_add_edge_invalid():
+    a = Vertex("AC")
+    assert_raises(TypeError, a.add_edge, "B")
