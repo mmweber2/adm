d5094075bb31af148b09c3aa2eebb8c17a512efe
Mary Ellen Weber <mmweber2@gmail.com>
2016-05-04 09:28:24 +0900

    Create initial subsets() function.

---
+++ permute/subsets.py
@@ -0,0 +1,24 @@
+def subsets(array):
+    """
+    Generates all of the subsets of array.
+
+    If the elements in array are not unique, duplicate elements will
+    be included the same number of times that they appear in array.
+
+    Args:
+        array: The iterable of items from which to create subsets.
+
+        remaining; The remaining items from which to create subsets.
+    """
+    sets = []
+    # Create subsets of one element
+    if len(array) == 1:
+        sets.extend([[], array[0]])
+        return sets
+    else:
+        smaller_sets = subsets(array[1:])
+        sets.extend(smaller_sets)
+        for subset in smaller_sets:
+            sets.append([[remaining[0]] + subset])
+        return sets
+
