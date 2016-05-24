1614691732fff50932788bb3fb4466abaa19eb54
Mary Ellen Weber <mmweber2@gmail.com>
2016-05-24 13:15:22 +0900

    Create tests for edit_distance.

---
+++ edit_distance/test_edit_distance.py
@@ -0,0 +1,20 @@
+from edit_distance import edit_distance
+from nose.tools import assert_equals
+
+def test_edit_same_string():
+    assert_equals(edit_distance("one", "one"), 0)
+
+def test_edit_one_empty_string():
+    assert_equals(edit_distance("", "one"), 3)
+
+def test_edit_two_empty_strings():
+    assert_equals(edit_distance("", ""), 0)
+
+def test_edit_one_swap():
+    assert_equals(edit_distance("cat", "car"), 1)
+
+def test_edit_one_del():
+    assert_equals(edit_distance("chat", "chaut"), 1)
+    
+def test_edit_longer_strings():
+    assert_equals(edit_distance("pineapple", "penelope"), 0)
