68250670dad8c67e5665552edad3584c065098b8
Mary Ellen Weber <mmweber2@gmail.com>
2016-05-30 13:44:46 +0900

    Update code per code review.
    Change recurrence section to keep a remaining_sum rather than calculate
    sum() every time.
    Change the partition reconstruction to use insert() instead of reversing
    the list at the end.

--- dp/partition2.py
+++ dp/partition2.py
@@ -23,10 +23,16 @@ def partition(seq, k):
         for i in xrange(1, len(seq)):
             # Value must be <= value for k-1 since there are more partitions
             min_maxes[i][j] = min_maxes[i][j-1]
+            # Keep track of the sum of the remaining numbers to avoid creating
+            # a new sum each time
+            remaining_sum = sum(seq[1:i+1])
             # Look at all partitions of seq up to j
             for x in xrange(i):
                 # Get the largest number we'll have if we put a partition here
-                local_max = max(min_maxes[x][j-1], sum(seq[x + 1:i + 1]))
+                local_max = max(min_maxes[x][j-1], remaining_sum)
+                # If we move the partition one forward, the remaining sum will
+                # be one element smaller
+                remaining_sum -= seq[x + 1]
                 if local_max <= min_maxes[i][j]:
                     min_maxes[i][j] = local_max
                     # x + 1 because that's where the sequence starts
@@ -37,14 +43,15 @@ def partition(seq, k):
     start = dividers[-1][-1]
     end = len(seq)
     while True:
-        split.append(seq[start:end])
+        # Starting at the end, so insert in reverse order
+        split.insert(0, seq[start:end])
         k -= 1
         if k == 0:
             break
         end = start
         # One partition has been added, so get the one for k - 1 partitions
         start = dividers[start - 1][k - 1]
-    print "Split was ", split[::-1]
+    print "Split was ", split
     print "Min max of this is ", min_maxes[-1][-1]
     return min_maxes[-1][-1]
 
