76f0fcd7cef2851aac2551c3b14a4b3b1defc925
Mary Ellen Weber <mmweber2@gmail.com>
2016-06-08 09:48:35 +0900

    Merge build_tree methods into __init__.
    Update tests to recognize this change.

--- trees/kd_tree.py
+++ trees/kd_tree.py
@@ -7,8 +7,8 @@ class KDTree(object):
     is contained in its own region.
 
     Once a tree has been created from data, the data will be sorted
-    among the k dimensions of the data. An approximation can then be found
-    for the nearest neighbor of a new point in O(height) time.
+    among the k dimensions of the data. The nearest neighbor of a new point
+    can then be found in O(height) time.
 
     The data is split first among the first dimension, then the second
     dimension, and so on until it has been split by the kth dimension, then
@@ -30,45 +30,19 @@ class KDTree(object):
         which may be None.
     """
 
-    def __init__(self, value, dimension):
-        """Create a new KD Tree subtree.
+    def __init__(self, data, dimension=1, error_checking=True):
+        """Create a new KD tree from a set of data.
 
         Each value contains all dimensions, so if k is 2 and value is
         (10, 5), the data point is (10, 5), and the tree will be sorted by
         one of those dimensions, as indicated by dimension below.
         The tree's k is equal to len(value).
 
-        Args:
-            value: The iterable of numbers this node represents.
-
-            dimension: Integer representing the dimension among which this Node
-            splits the data, or by which it has been split if it is a leaf Node.
-            Dimensions are 1-indexed, so 1 and 2 are the acceptable dimensions
-            for a 2-dimensional tree.
-            Must be in the range 0 < dimension <= k.
-
-        Raises:
-            ValueError: dimension is outside the valid range.
-
-            TypeError: value is not an iterable.
-        """
-        # Values should not be modified once they're added to the tree
-        self.value = tuple(value)
-        if dimension <= 0 or dimension > len(value):
-            raise ValueError("dimension must be in range 0 < dimension <= k")
-        self.dimension = dimension
-        self.left = None
-        self.right = None
-    
-    # TODO: Incorporate this into __init__.
-    @staticmethod
-    def build_tree(data):
-        """Create a new KD tree from a set of data.
-
         Each position represents a dimension, so the first item always
         represents the first dimension, the second a second dimension,
         and so on. Each list or tuple must contain the same number of
         dimensions.
+
         Items must all be of the same type (numbers, strings, etc), as the
         default comparator will be used to determine their ordering.
         Items must not be changed after creating the tree in order for
@@ -78,42 +52,49 @@ class KDTree(object):
             data: List or tuple of k-length list or tuples, where
             k is an integer greater than 1.
 
-        Returns:
-            A KDTree object as described in the class documentation.
+            dimension: Integer representing the dimension among which this Node
+            splits the data, or by which it has been split if it is a leaf Node.
+            Dimensions are 1-indexed, so 1 and 2 are the acceptable dimensions
+            for a 2-dimensional tree.
+            Must be in the range 0 < dimension <= k.
 
         Raises:
-            ValueError: data points contain an invalid number of
-            dimensions.
-
             IndexError: data contains data points of different lengths
             (dimensions).
-        """
-        k = len(data[0])
-        if k == 1:
-            raise ValueError("Data values must have 2 or more dimensions")
-        # Explicit check for uneven dimensions because the behavior may be
-        # different depending on the position of the longer or shorter item
-        for item in data[1:]:
-            if len(item) != k:
-                raise IndexError("All items must contain k dimensions")
-        return KDTree._build_tree_inner(data[:], 1)
 
-    # TODO: Incorporate this into __init__
-    @staticmethod
-    def _build_tree_inner(data, dimension):
-        """Create a new subtree for a KD tree."""
-        if data == []:
-            return None
-        median = KDTree._get_median_index(data, dimension)
-        # _get_median_index sorts by dimension, so we can index to the median
-        root = KDTree(data[median], dimension)
+            ValueError: dimension is outside the valid range.
+        """
         # All points must have the same number of dimensions
         k = len(data[0])
+        if error_checking:
+            if k == 1:
+                raise ValueError("Data values must have 2 or more dimensions")
+            # Explicit check for uneven dimensions because the behavior may be
+            # different depending on the position of the longer or shorter item
+            for item in data[1:]:
+                if len(item) != k:
+                    raise IndexError("All items must contain k dimensions")
+        if dimension <= 0 or dimension > k:
+            raise ValueError("dimension must be in range 0 < dimension <= k")
+        self.left = None
+        self.right = None
+        self.dimension = dimension
+        # Don't recurse if node has no children
+        if len(data) == 1:
+            self.value = tuple(data[0])
+            return
+        median = KDTree._get_median_index(data, dimension)
+        # _get_median_index sorts by dimension, so we can index to the median
+        # Values should not be modified once they're added to the tree
+        self.value = tuple(data[median])
         dimension = KDTree._get_next_dimension(k, dimension)
         # _get_median sorts by this dimension, so we can divide data
-        root.left = KDTree._build_tree_inner(data[:median], dimension)
-        root.right = KDTree._build_tree_inner(data[median + 1:], dimension)
-        return root
+        left_data = data[:median]
+        right_data = data[median + 1:]
+        if left_data:
+            self.left = KDTree(left_data, dimension, False)
+        if right_data:
+            self.right = KDTree(right_data, dimension, False)
 
     @staticmethod
     def _get_next_dimension(k, dimension):
@@ -138,6 +119,7 @@ class KDTree(object):
         Dimension: (dimension sorted by for that split)
 
         """
+        print "---"
         queue = [self]
         while len(queue) > 0:
             current = queue.pop(0)
@@ -167,7 +149,7 @@ class KDTree(object):
         """
         dimension = 1
         subtree = self
-        closest_value = subtree.value
+        closest_value = self.value
         while True:
             # No further divisions, so this must be the closest existing value
             if subtree == None:
--- trees/test_kd_tree.py
+++ trees/test_kd_tree.py
@@ -2,48 +2,41 @@ from kd_tree import KDTree
 from nose.tools import assert_raises
 from nose.tools import assert_equals
 
-def test_build_tree_2k():
+def test_construct_2k():
     print "K = 2:"
     data = [(30, 40), (5, 25), (10, 12), (70, 70), (50, 30), (35, 45)]
-    KDTree.build_tree(data).print_tree()
+    KDTree(data).print_tree()
 
-def test_build_tree_inconsistent_k():
+def test_construct_inconsistent_k():
     data = [(30, 40), (5, 25, 3)]
-    assert_raises(IndexError, KDTree.build_tree, data)
+    assert_raises(IndexError, KDTree, data)
     data = [(30, 40, 1), (5, 25)]
-    assert_raises(IndexError, KDTree.build_tree, data)
+    assert_raises(IndexError, KDTree, data)
 
-def test_build_tree_3k():
+def test_construct_3k():
     print "K = 3:"
     data = [
         (30, 40, 10), (5, 25, 2), (10, 12, 30), (70, 70, 10), (50, 30, 5), 
         (35, 45, 15)]
-    KDTree.build_tree(data).print_tree()
+    KDTree(data).print_tree()
 
-def test_build_tree_single_point():
+def test_construct_single_point():
     print "K = 2, single point"
     data = [(10, 15)]
-    KDTree.build_tree(data).print_tree()
+    KDTree(data).print_tree()
 
-def test_build_tree_1k():
-    assert_raises(ValueError, KDTree.build_tree, [(1,), (2,)])
+def test_construct_1k():
+    assert_raises(ValueError, KDTree, [(1,), (2,)])
 
-def test_kd_construct_valid():
-    tree = KDTree((10, 15), 1)
-    assert tree.value == (10, 15)
-    assert tree.dimension == 1
-    tree = KDTree((10, 15), 2)
-    assert tree.dimension == 2
-    
 def test_kd_construct_zero():
-    assert_raises(ValueError, KDTree, (3, 4), 0)
+    assert_raises(ValueError, KDTree, ((3, 4), ), 0)
 
 def test_kd_construct_non_iterable():
     assert_raises(TypeError, KDTree, 2, 1)
 
 def test_kd_find_closest():
     data = [(30, 40), (5, 25), (10, 12), (70, 70), (50, 30), (35, 45)]
-    tree = KDTree.build_tree(data)
+    tree = KDTree(data)
     assert_equals(tree.find_closest((0, 0)), (10, 12))
     assert_equals(tree.find_closest((0, 100)), (30, 40))
     assert_equals(tree.find_closest((50, 50)), (50, 30))
