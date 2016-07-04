4da9c2958c2f812c9c734164f640b2d5b789c33c
Mary Ellen Weber <mmweber2@gmail.com>
2016-07-04 12:39:48 -0500

    Create new version of divide, still in progress.

--- arithmetic/divide.py
+++ arithmetic/divide.py
@@ -1,15 +1,39 @@
 from integer import BigInteger
+from multiply import multiply
 
-def divide(a, b):
-    """Returns the quotient of a divided by b.
+def divide(dividend, divisor):
+    """Returns the integer divided quotient of a divided by b.
 
     Args:
-        a: The BigInteger to be divided; the dividend.
-        b: The BigInteger to divide by; the divisor. May not be 0.
+        dividend: The BigInteger to be divided.
+        divisor: The BigInteger to divide by; the divisor. May not be 0.
 
     Returns:
-        A new BigInteger representing the quotient of a and b (a divided by b).
+        A new BigInteger representing the quotient of dividend and divisor.
+        Since this function performs integer division, any remainder will be
+        stripped (i.e., 3 divided by 2 equals 1).
 
     Raises:
-        ValueError: b is equal to 0.
+        ZeroDivisionError: divisor is equal to 0.
     """
+    if divisor == BigInteger("0"):
+        raise ZeroDivisionError("Cannot divide by zero")
+    quotient = BigInteger("0")
+    # Create table of divisor products for lookup
+    product_table = []
+    multiplicant = BigInteger("1")
+    while True:
+        product = multiply(multiplicant, divisor)
+        product_table.append((multiplicant, product))
+        if product > divisor:
+            break
+        multiplicant = multiply(multiplicant, BigInteger("2"))
+    remainder = dividend
+    # Start with biggest product less than dividend
+    for i in xrange(len(product_table), -1, -1):
+        factor, product = product_table[i]
+        if product < remainder:
+            quotient = BigInteger.add(quotient, factor)
+            remainder = BigInteger.subtract(remainder, product)
+        # Keep checking smaller products whether or not we found a match
+    return quotient
