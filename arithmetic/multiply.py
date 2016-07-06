8dcbcf7507d0800f148d06bfda445b0fb113d1e1
Mary Ellen Weber <mmweber2@gmail.com>
2016-07-06 08:43:08 -0500

    Change multiply per code review.
    Change multiply such that it creates one BigInteger for every digit of
    the first number, rather than one BigInteger for every sub-product (each
    digit of the first number times each digit of the second number).

--- arithmetic/multiply.py
+++ arithmetic/multiply.py
@@ -15,19 +15,25 @@ def multiply(a, b):
     if len(a.digits) > len(b.digits): a, b = b, a
     digit = 0
     products = []
+    carry = 0
     while digit < len(a.digits):
-        multiplicant = a.digits[-1 - digit]
+        multiplicant = a.digits[-1 -digit]
+        products.append([])
         for i in xrange(len(b.digits)):
             product = multiplicant * b.digits[-1 -i]
-            # Don't bother adding zero products
-            if product == 0: continue
-            # Move the product over the correct number of zeroes
-            zeroes = "0" * (i + digit)
-            products.append(BigInteger(str(product) + zeroes))
+            product += carry
+            # Add smallest digit to list (to turn into final answer later)
+            products[-1].insert(0, product % 10)
+            carry = product / 10
         digit += 1
+        # If carry is 0, it will be ignored when turning into a BigInteger
+        products[-1].insert(0, carry)
+        # Add zeroes to allow for position of multiplicant digit
+        products[-1].extend([0] * (digit - 1))
     result = BigInteger("0")
     for product in products:
-        result = BigInteger.add(result, product)
+        partial_result = BigInteger("".join([str(digit) for digit in product]))
+        result = BigInteger.add(result, partial_result)
     if negative:
         return result._flip_sign()
     return result
