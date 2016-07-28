cf8ff4518e3308a270c37dfb0759111abdea4ed7
Mary Ellen Weber <mmweber2@gmail.com>
2016-07-28 12:33:58 -0500

    Change get_min_edges per code review.
    Change wording and variable names in Kruskal's for clarity.
    Add TODOs to Kruskal's regarding the list of linked vertices for each
    vertex.
    Change variable names in Prim's for clarity.
    Remove the 'possible edges' list and just traverse the edges connected
    to each vertex in the forest.

--- span/mst.py
+++ span/mst.py
@@ -6,42 +6,79 @@ from operator import attrgetter
 # Points aren't start and end because they're undirected
 Edge = namedtuple('Edge', ('vertex1', 'vertex2', 'value'))
 
-# TODO: complete and test
 class SpanningTree(object):
     """A representation of a spanning tree."""
+    # Share known vertices and total weight among all of the nodes
+    vertices = {}
+    total_weight = 0
 
-    # TODO: What are allowable values?
-    # TODO: Include root?
-    def __init__(self, value):
-        """Creates a new SpanningTree vertex with no edges.
+    def __init__(self, name, weight):
+        """Creates a new SpanningTree node with no edges.
         
         Args:
-            value: The value to assign to this vertex. May be a number or
+            name: The name to assign to this node. May be a number or
                 string. 
-            root: boolean indicating whether this vertex is the root.
-                Defaults to False.
+            weight: Float, the weight between this node and its parent.
+                Should be 0 for the root, or a positive number otherwise.
         """
-        self.value = value
-        self.edges = []
+        self.name = name
+        self.weight = weight
+        self.total_weight += weight
+        self.children = []
+        self.vertices[name] = self
 
-    def add_edge(self, edge):
-        """Adds a new edge to this vertex.
+    def add_child(self, edge):
+        """Adds a new node to the Spanning Tree.
 
         Args:
-            edge: A namedtuple Edge to add.
+            edge: A namedtuple Edge to add. Must connect a node in the
+                tree with a new node not yet in the tree.
+
+        Raises:
+            ValueError: Edge links two nodes already in the SpanningTree,
+                or Edge links two nodes and neither is in the SpanningTree.
         """
-        self.edges.append(edge)
+        # Expect format (old, new) by default and swap if opposite order
+        parent = edge.vertex1
+        child = edge.vertex2
+        if child in self.vertices:
+            if parent in self.vertices:
+                raise ValueError("Edge links two nodes already in tree")
+            parent, child = child, parent
+        elif parent not in self.vertices:
+            # Neither vertex is in the SpanningTree
+            raise ValueError("Edge must contain one node already in tree")
+        new_vertex = SpanningTree(child, edge.value)
+        self.vertices[parent].children.append(new_vertex)
 
-    # TODO
-    def get_total_weight(self):
-        """Returns the sum of the weights of the edges in this spanning tree."""
-        return 0
+    # TODO: Complete
+    def __str__(self):
+        """Returns a string representation of this SpanningTree.
 
-# TODO: Prim's gives us the edges in the order we want. Does Kruskal's?
+        Represents the tree in level order as follows:
+        Root: (root name)
+        Level 1: [((name of child), weight = <weight>) for child in children]
+        Level 2: [((name of child), weight = <weight>) for child in children of each Level 1 child]
+        .....
+        """ 
+
+# TODO: What to do if the list of edges contains non-minimum edges?
 def build_mst(edges):
-    root = SpanningTree(edges[0][0])
-    for edge in edges:
+    """Builds a Minimum Spanning Tree using the provided edges.
+    
+    Args:
+        edges: A list of edges with the minimum sum of weights to connect
+            all vertices, as from get_min_edges_kruskal or get_min_edges_prim.
 
+    Returns:
+        A SpanningTree connected by the edges in edges.
+    """
+    # 0 distance to get from root to root
+    root = SpanningTree(edges[0][0], 0)
+    # Logic for handling edges and error checking is in add_child()
+    for edge in edges:
+        root.add_child(edge)
+    return root
 
 def _is_connected(edges):
     # Avoid IndexError when choosing a vertex to start from
@@ -75,7 +112,7 @@ def get_min_edges_kruskal(edges):
 
     Returns:
         A list of edges connecting all vertices with the minimum sum of weights,
-        in order of ascending weight.
+            in order of ascending weight.
 
     Raises:
         ValueError: At least one edge is disconnected (unreachable) from other
@@ -85,23 +122,28 @@ def get_min_edges_kruskal(edges):
         raise ValueError("Graph must be a single connected component")
     edges.sort(key=attrgetter('value'))
     # We are only building a subset of the graph, so don't add any edges yet
-    connections = defaultdict(set)
-    subgraph = []
+    forest = defaultdict(set)
+    mst_edges = []
     for edge in edges:
+        # TODO: Instead of attaching every vertex to every vertex it's connected
+        # to, get rid of the individual vertices in the defaultdict and add new
+        # vertices that merge them (ex. AB when merging A and B).
+        # This will mean that we need to go through all forests to find the
+        # vertices we are looking to merge.
         v1, v2 = edge.vertex1, edge.vertex2
         # Can check these in either order, we will add both
-        if v1 in connections[v2]:
+        if v1 in forest[v2]:
             # Discard edge, points already connected
             continue
         # Merge connections of both vertices
-        connections[v1].add(v2)
-        for point in connections[v1]:
-            connections[point].update(connections[v1])
-        connections[v2].add(v1)
-        for point in connections[v2]:
-            connections[point].update(connections[v2])
-        subgraph.append(edge)
-    return subgraph
+        forest[v1].add(v2)
+        for vertex in forest[v1]:
+            forest[vertex].update(forest[v1])
+        forest[v2].add(v1)
+        for vertex in forest[v2]:
+            forest[vertex].update(forest[v2])
+        mst_edges.append(edge)
+    return mst_edges
 
 def get_min_edges_prim(edges):
     """Returns a list of edges to connect all vertices with a minimum sum of
@@ -125,34 +167,34 @@ def get_min_edges_prim(edges):
         raise ValueError("Graph must be a single connected component")
     if len(edges) == 0:
         return []
-    connections = defaultdict(list)
+    # Map of vertices to list of edges that involve them
+    graph = defaultdict(list)
     for edge in edges:
-        start, end = edge.vertex1, edge.vertex2
         # Add edges instead of points because we need to be able to look up the
         # edges when we add them to grow the components
-        connections[start].append(edge)
-        connections[end].append(edge)
+        graph[edge.vertex1].append(edge)
+        graph[edge.vertex2].append(edge)
     # Vertices included so far
-    components = set()
-    tree_edges = []
+    forest = set()
+    mst_edges = []
     # Pick arbitrary vertex to start from
-    components.add(edges[0][0])
-    while len(components) < len(connections):
+    forest.add(edges[0].vertex1)
+    while len(forest) < len(graph):
         # Get all edges that connect to points in the component
-        possible_edges = []
-        for point in components:
-            possible_edges.extend(connections[point])
         best_edge = None
-        for edge in possible_edges:
-            # We don't know which end of the edge is connected to our component,
-            # but one of them must be. If both are connected, discard it.
-            if edge.vertex1 in components and edge.vertex2 in components:
-                continue
-            # Edge connects a new vertex to the component; keep it
-            if (not best_edge) or (edge.value < best_edge.value):
-                best_edge = edge
-        tree_edges.append(best_edge)
-        # One of these vertices is already in components, but add both
+        for vertex in forest:
+            for edge in graph[vertex]:
+                # We don't know which end of the edge is connected in
+                # forest, but one of them must be.
+                #If both are connected, discard it.
+                if edge.vertex1 in forest and edge.vertex2 in forest:
+                    continue
+                # Edge connects a new vertex to the forest; keep it
+                if (not best_edge) or (edge.value < best_edge.value):
+                    best_edge = edge
+        # Shortest edge that connects the forest to a new element; keep it
+        mst_edges.append(best_edge)
+        # One of these vertices is already in forest, but add both
         # to the set to ensure the new one is added
-        components.update(best_edge.vertex1, best_edge.vertex2)
-    return tree_edges
+        forest.update(best_edge.vertex1, best_edge.vertex2)
+    return mst_edges
