1666f99823c64fbc220af4a64dd0ee5e5a6646f4
Mary Ellen Weber <mmweber2@gmail.com>
2016-07-29 08:51:22 -0500

    Add large random test to test_merge.

--- sort/test_merge.py
+++ sort/test_merge.py
@@ -1,4 +1,5 @@
 from merge import merge_sort
+from random import sample
 from nose.tools import assert_equals
 
 def test_merge_empty():
@@ -18,5 +19,11 @@ def test_merge_backwards():
 
 def test_merge_duplicates():
     assert_equals(merge_sort([1, 4, 1, 3, 2, 4]), [1, 1, 2, 3, 4, 4])
+
+# Sample does not create duplicates, but we've already confirmed that
+# the sort handles duplicates in the previous test.
+def test_merge_large():
+    input_list = sample(xrange(100000), 90)
+    assert_equals(merge_sort(input_list), sorted(input_list))
 
 
