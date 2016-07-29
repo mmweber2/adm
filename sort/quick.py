0eb19ea2d052121e17f8127bf6876cf04edf4681
Mary Ellen Weber <mmweber2@gmail.com>
2016-07-29 09:13:07 -0500

    Change quicksort to only accept a list as input.

--- sort/quick.py
+++ sort/quick.py
@@ -1,25 +1,20 @@
 def quicksort(sequence):
-    """Returns a sorted list of the data in sequence.
+    """Returns a sorted copy of a list.
 
     Args:
-        sequence: An iterable of data to sort. May be a list, tuple, string,
-            or other sequence of comparable objects.
+        sequence: A list of data to sort. Items may be numbers, strings,
+            or other comparable objects.
 
     Returns:
-        A list of the data in sequence in non-descending lexographic order.
+        A list copy of sequence in non-descending lexographic order.
     """
     if len(sequence) <= 1:
-        return list(sequence)
+        return sequence
     mid = len(sequence) / 2
-    # TODO:
-    # Pick three pivots and take the median
-    # Choosing the first, last, and middle element should help us avoid
-    # a bad pivot, especially if sequence is already sorted
-    pivot = sequence[mid]
+    pivot = _median_of_three(sequence)
     smaller_elements = []
     larger_elements = []
-    # Don't sort the pivot
-    for element in sequence[:mid] + sequence[mid + 1:]:
+    for element in sequence[:mid] + sequence[mid + 1:]: # Add the pivot later
         if element <= pivot:
             smaller_elements.append(element)
         else:
@@ -28,6 +23,23 @@ def quicksort(sequence):
     larger_elements = quicksort(larger_elements)
     return smaller_elements + [pivot] + larger_elements
 
-
-
-
+def _median_of_three(sequence):
+    """Helper method for quicksort to pick median of 3 elements."""
+    # Besides just returning the median of the first, middle, and last elements,
+    # this function sorts them to help with future sub-sorts.
+    # If sequence is only 2 elements, the "middle" and last element will be the
+    # same, and since neither is less than the other, they will be left as is.
+    # If sequence is only one element, all three elements will be the same,
+    # but this should not happen, because quicksort returns before choosing
+    # a pivot if the list is only a single element.
+    mid = len(sequence) / 2
+    # Sort first and last element
+    if sequence[-1] < sequence[0]:
+        sequence[-1], sequence[0] = sequence[0], sequence[-1]
+    # Sort last element and middle element
+    if sequence[-1] < sequence[mid]:
+        sequence[-1], sequence[mid] = sequence[mid], sequence[-1]
+    # Sort middle element and first element
+    if sequence[mid] < sequence[0]:
+        sequence[0], sequence[mid] = sequence[mid], sequence[0]
+    return sequence[mid]
