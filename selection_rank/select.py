c202848d5205ff040dba7dffd85985b0f85a5853
Mary Ellen Weber <mmweber2@gmail.com>
2016-08-03 18:19:18 -0500

    Redo get_rank (selection rank) to be in place.

--- selection_rank/select.py
+++ selection_rank/select.py
@@ -1,15 +1,22 @@
-import random
-def get_rank(array, k):
+from time import sleep
+
+def get_rank(array, k, start=0, end=None):
     """Returns the kth smallest item in array.
 
     Args:
         array: list of comparable items to search. Does not need to be in sorted
             order, and duplicate elements are permitted.
+            This function modifies the list.
 
         k: integer, the ranking of the item to return.
             For example, if you were looking for the median of an odd number
             of items, k would be len(array) / 2.
             Must be in the range 0 <= k < len(array).
+
+        start: integer, the index at which to begin ranking. Defaults to 0.
+
+        end: integer, the index at which to end ranking (inclusive).
+            Defaults to the last item in array.
 
     Returns:
         The item in array that belongs at the kth index if array were fully
@@ -18,29 +25,36 @@ def get_rank(array, k):
     Raises:
         IndexError: k is not a valid index for array.
     """
-    if len(array) == 0:
-        return None
-    if k < 0 or k >= len(array):
-        raise IndexError("k must be a valid index for array")
-    pivot = random.choice(array)
-    # Elements in array <= pivot
-    smaller = []
-    greater = []
-    for element in array:
-        if element < pivot:
-            smaller.append(element)
-        else:
-            greater.append(element)
-    if k == len(smaller):
-        # pivot was the kth item
-        # This will always be the case if len(array) = 1
-        return pivot
-    if k == len(smaller) - 1:
-        # k is largest element in smaller
-        return get_rank(smaller, len(smaller) - 1)
-    if k < len(smaller):
-        # k is somewhere in smaller
-        return get_rank(smaller, k)
+    if end is None:
+        end = len(array) - 1
+    if k < 0 or k > end - start:
+        raise IndexError("k must be a valid index for this array")
+    # Only one item in this partition
+    if start == end and k == 0:
+        return array[start]
+    # Absolute pivot position (across all of array)
+    abs_pivot = partition(array, start, end)
+    # Relative pivot position; size of this left partition
+    left_size = abs_pivot - start
+    if k == left_size:
+        # Found the exact index of k
+        return array[abs_pivot]
+    if k < left_size:
+        # Recurse on left side of partition, exclude pivot itself
+        return get_rank(array, k, start, abs_pivot - 1)
     else:
-        # k is somewhere in greater, adjust k to note the smaller elements
-        return get_rank(greater, k - len(smaller))
+        # Recurse on right side of partition, exclude pivot itself
+        # Adjust k to account for elements on left (including pivot)
+        return get_rank(array, k - left_size - 1, abs_pivot + 1, end)
+
+def partition(array, start, end):
+    """Helper function for get_rank."""
+    pivot = array[end]
+    swap_pos = start
+    for i in xrange(start, end):
+        if array[i] <= pivot:
+            array[swap_pos], array[i] = array[i], array[swap_pos]
+            swap_pos += 1
+    array[swap_pos], array[end] = array[end], array[swap_pos]
+    return swap_pos
+
