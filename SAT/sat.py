801afcebfd41f81b901eec03ec4a3f5d024b3e1f
Mary Ellen Weber <mmweber2@gmail.com>
2016-08-05 10:06:36 -0500

    Write working version of SAT using hill climbing.

--- SAT/sat.py
+++ SAT/sat.py
@@ -1,31 +1,39 @@
+import random
 def three_sat(clauses):
-    """Returns True if all clauses in clauses are satisfiable.
+    """Returns True if all clauses are satisfiable.
 
     Args:
-        clauses: a list of tuples where each tuple contains up to three string
-            literals, each of which represents a variable to satisfy.
+        clauses: a list of strings where each string contains up to three
+            literals separated by spaces, and each literal represents a variable
+            to satisfy.
             Negated literals are marked with a preceding !.
             For example:
-            [(A, B, !C), (B, C), (!B), (!A, C)]
+            ["A B !C", "B C", "!B", "!A C"]
 
     Returns:
         True iff there exists an assignment for all variables in clauses such
-            that all clauses can be satisfied.
+            that all clauses can be satisfied, or if clauses is empty.
 
     Raises:
         ValueError: At least one clause in clauses contains more than three
             literals.
     """
+    # Based on Python returning True for all([]), treat empty list as True
+    if not clauses:
+        return True
     # Start with 0 so we can 1-index and not worry about -0
     literals = [0]
-    # Literal itself ('a') to index (0)
+    # Literal itself ('a') to index (1)
     literals_table = {}
     # Translate each clause
-    assignments = []
+    parsed_clauses = []
     # Collect all unique literals
     for clause in clauses:
         parsed_clause = []
-        for literal in clause.split():
+        clause = clause.split()
+        if len(clause) > 3:
+            raise ValueError("clause {} contains >3 literals".format(clause))
+        for literal in clause:
             # Don't treat negated literals as separate entities
             negated = 1 if literal[0] == "!" else 0
             literal = literal[negated:]
@@ -36,14 +44,42 @@ def three_sat(clauses):
                 parsed_clause.append(0 - literals_table[literal])
             else:
                 parsed_clause.append(literals_table[literal])
-        assignments.append(parsed_clause)
-    print "Literals are ", literals
-    print "Literals table is ", literals_table
-    print "Assignments are ", assignments
+        parsed_clauses.append(parsed_clause)
+    # Track whether each literal is True or False. Set all to True by default.
+    assignments = {literal:True for literal in literals_table.values()}
+    unsatisfied = check_if_satisfied(parsed_clauses, assignments)
+    current_best = len(unsatisfied)
+    # TODO: At what point should we break and give up looking?
+    for _ in xrange(10000):
+        if current_best == 0:
+            return True
+        # Permute a random assignment and see if it improves
+        swap = random.choice(xrange(1, len(literals)))
+        assignments[swap] ^= True
+        new_score = len(check_if_satisfied(parsed_clauses, assignments))
+        if new_score <= current_best:
+            current_best = new_score
+            unsatisfied = check_if_satisfied(parsed_clauses, assignments)
+        else:
+            # Undo the swap since it did not improve
+            assignments[swap] ^= True
+    # Couldn't find a perfect match, so return the best we found
+    return False
 
-array = [("A B !C"), ("B C"), ("!B"), ("!A C")]
-print "Array is ", array
-three_sat(array)
+def check_if_satisfied(clauses, assignments):
+    """Helper function for three_sat to check all clauses."""
+    unsatisfied = []
+    for i in xrange(len(clauses)):
+        if not is_satisfied(clauses[i], assignments):
+            unsatisfied.append(i)
+    return unsatisfied
 
-
-
+def is_satisfied(clause, assignments):
+    """Helper function for three_sat to check a single clause."""
+    outcome = False
+    for literal in clause:
+        negated = False
+        if literal < 0:
+            negated = True
+        outcome |= (assignments[abs(literal)] ^ negated)
+    return outcome
