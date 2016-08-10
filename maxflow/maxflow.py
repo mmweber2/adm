4f8def29f2fc0f89cd8e3b83947a6ff3fea68d2a
Mary Ellen Weber <mmweber2@gmail.com>
2016-08-10 14:05:27 -0500

    Work on maxflow, it now passes the large test.

--- maxflow/maxflow.py
+++ maxflow/maxflow.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from time import sleep
 
 def max_flow(edges, source, sink):
     """Returns the maximum flow that can be routed from source to sink.
@@ -14,22 +15,8 @@ def max_flow(edges, source, sink):
             If both names are not included in edges, the maximum flow will be 0.
     """
     # TODO: Return the path and amount of capacity through each, or just the total capacity?
-    # Edge format: (start, end, capacity)
-    outgoing_edges = defaultdict(list)
-    incoming_edges = defaultdict(list)
-    for edge in edges:
-        start, end, capacity = edge
-        outgoing_edges[start].append(edge)
-        incoming_edges[end].append(edge)
-    # Initially vertex labeling
-    labels = {vertex:0 for vertex in outgoing_edges}
-    labels[source] = len(outgoing_edges)
-    flow = {edge:0 for edge in edges}
-    # Get an arbitrary edge from the source node
-    first_edge = outgoing_edges[source[0]]
-    # TODO: Why does the source start flowing to its first edge?
-    flow[first_edge] = first_edge[2]
-    excess_nodes = [first_edge[2]]
+    flow, labels, outgoing_edges, incoming_edges = initialize(edges, source)
+    excess_nodes = [edge[1] for edge in outgoing_edges[source]]
     while len(excess_nodes) > 0:
         current = excess_nodes.pop()
         pushed = False
@@ -37,21 +24,50 @@ def max_flow(edges, source, sink):
             v, w, capacity = edge
             if labels[w] < labels[v]:
                 # Push flow to edge
-                v_excess = excess(v, outgoing_edges, incoming_edges)
-                flow[edge] += min(v_excess, capacity)
-                pushed = True
-                break
+                v_excess = excess(flow, outgoing_edges[v], incoming_edges[v])
+                print "Trying to push excess {} to edge {}".format(v_excess, edge)
+                push_amount = min(v_excess, capacity-flow[edge])
+                if push_amount <= 0:
+                    print "Could not push more"
+                else:
+                    flow[edge] += push_amount
+                    # Remove excess from outgoing edge
+                    pushed = True
+                    break
         if not pushed:
-            relabel(current, outgoing_edges[current], labels)
+            if not relabel(current, outgoing_edges[current], labels):
+                break
         excess_nodes = [node for node in outgoing_edges if excess(
-            node, outgoing_edges, incoming_edges) > 0]
-    return sum(flow[x] for x in outgoing_edges[source]) 
+            flow, outgoing_edges[node], incoming_edges[node]) > 0
+            # once filled, sink will always have positive excess
+            and node is not sink]
+    return sum(flow[x] for x in incoming_edges[sink]) 
 
-def excess(v, outgoing_edges, incoming_edges):
-    return incoming_edges[v][2] - outgoing_edges[v][2]
+def initialize(edges, source):
+    outgoing_edges = defaultdict(list)
+    incoming_edges = defaultdict(list)
+    for edge in edges:
+        start, end, capacity = edge
+        outgoing_edges[start].append(edge)
+        incoming_edges[end].append(edge)
+    all_vertices = outgoing_edges.keys() + incoming_edges.keys()
+    labels = {vertex:0 for vertex in all_vertices}
+    labels[source] = len(all_vertices)
+    flow = {edge:0 for edge in edges}
+    # Initialize all source edges to have maximum flow
+    for source_edge in outgoing_edges[source]:
+        flow[source_edge] = source_edge[2]
+    return flow, labels, outgoing_edges, incoming_edges
+
+def excess(flow, outgoing, incoming):
+    out_flow = sum(flow[edge] for edge in outgoing)
+    in_flow = sum(flow[edge] for edge in incoming)
+    return in_flow - out_flow
 
 def relabel(v, v_edges, labels):
     for edge in v_edges:
         v, w, capacity = edge
         if labels[w] >= labels[v]:
             labels[v] += 1
+            return True
+    return False
