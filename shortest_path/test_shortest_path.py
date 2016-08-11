dabbdc6da4e1b2ab4a64a84b74762148d7beb332
Mary Ellen Weber <mmweber2@gmail.com>
2016-08-11 10:52:22 -0500

    Write initial tests for shortest path.
    Add tests:
    Edges is empty (so source is not in edges).
    Edges contains a single valid edge from source.
    Edges is non-empty, but has no edge from source.
    There are two valid paths from source, one shorter than the other.
    Two paths from source converge later in the graph.

---
+++ shortest_path/test_shortest_path.py
@@ -0,0 +1,20 @@
+from shortest_path import shortest_path
+from nose.tools import assert_equals
+
+def test_empty_edges():
+    assert_equals(shortest_path([], "A"), {"A":0})
+
+def test_single_edge():
+    assert_equals(shortest_path([("A", "B", 2)], "A"), {"A":0, "B":2})
+
+def test_no_path():
+    assert_equals(shortest_path([("A", "B", 2)], "B"), {"A":float("inf"), "B":0})
+
+def test_shorter_of_two_paths_from_start():
+    paths = [(1, 2, 5), (1, 2, 2)]
+    assert_equals(shortest_path(paths, 1), {1:0, 2:2})
+
+def test_shorter_of_two_paths_split():
+    paths = [(1, 2, 5), (2, 3, 4), (2, 4, 2), (4, 5, 4), (3, 5, 1)]
+    assert_equals(shortest_path(paths, 1), {1:0, 2:5, 3:9, 4:7, 5:10})
+
