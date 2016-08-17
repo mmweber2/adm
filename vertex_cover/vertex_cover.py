7f1f07d4a39097ad126859ba3b2701f70af34164
Mary Ellen Weber <mmweber2@gmail.com>
2016-08-17 14:47:02 -0500

    Make some optimizations to cover.
    Instead of choosing a random edge, create a function that looks for the
    edge that affects the most vertices.
    Don't add vertices to the cover if they only have one edge, unless the
    edge is the only one connected to either vertex.
    Keep track of 'covered' vertices that are directly connected to the
    cover by an edge. These vertices don't need to be catered to when
    looking for new edges.
    Create a function that returns the number of uncovered vertices adjacent
    to a vertex.

--- vertex_cover/vertex_cover.py
+++ vertex_cover/vertex_cover.py
@@ -2,8 +2,10 @@ from collections import defaultdict
 def cover(edges):
     """Returns a vertex cover of the vertices in edges.
     
-    Returns a maximal matching subset of vertices listed in edges such that each
+    Returns a maximal matching subset of the vertices in edges such that each
         edge is incident to at least one vertex in the subset.
+    Uses a greedy algorithm that finds a cover at most twice as large as the
+        optimal cover.
 
     Args:
         edges: A list of tuples in the form (v1, v2), where v1 and v2 are
@@ -25,12 +27,44 @@ def cover(edges):
         graph[start].append(edge)
         graph[end].append(edge)
     remaining_edges = edges[:]
+    # Result vertex cover
     cover = set()
+    # Vertices 'covered' through an edge to a vertex in cover or cover itself
+    covered = set()
     while len(remaining_edges) > 0:
-        current = remaining_edges.pop()
-        cover.update(edge)
+        current = _find_best_edge(remaining_edges, graph, covered)
+        if _get_vertex_count(graph, current[0], covered) < 2:
+            # Avoid adding vertices with 1 edge, unless both have only one edge
+            cover.add(current[1])
+        elif _get_vertex_count(graph, current[1], covered) < 2:
+            cover.add(current[0])
+        else:
+            cover.update(current)
+        # Both should now be covered, regardless of which were added to cover
+        covered.update(current)
         adj_edges = set(graph[current[0]] + graph[current[1]])
+        # Filter out any edges that touch a vertex in current
         remaining_edges = [x for x in remaining_edges if x not in adj_edges]
     return cover
 
-        
+def _find_best_edge(edges, graph, covered):
+    """Returns the edge that touches the most remaining vertices."""
+    max_edges = 0
+    best_edge = edges[0]
+    for edge in edges:
+        start, end = edge
+        affects = sum(_get_vertex_count(graph, v, covered) for v in edge)
+        if affects > max_edges:
+            max_edges = affects
+            best_edge = edge
+    return best_edge
+
+def _get_vertex_count(graph, vertex, covered):
+    """Returns the number of vertices that are connected to this vertex."""
+    adj_vertices = []
+    for edge in graph[vertex]:
+        if edge[0] == vertex:
+            adj_vertices.append(edge[1])
+        else:
+            adj_vertices.append(edge[0])
+    return sum(1 for x in adj_vertices if x not in covered)
