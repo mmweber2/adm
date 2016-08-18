b80f7f5c647e5eaed0b743c6469f3cb8a3fd6baa
Mary Ellen Weber <mmweber2@gmail.com>
2016-08-18 16:32:22 -0500

    Add function is_valid_cover.
    Add new function is_valid_cover to test validity of a given cover.
    Update documentation of cover for accuracy.

--- vertex_cover/vertex_cover.py
+++ vertex_cover/vertex_cover.py
@@ -5,7 +5,8 @@ def cover(edges):
     Returns a maximal matching subset of the vertices in edges such that each
         edge is incident to at least one vertex in the subset.
     Uses a greedy algorithm that finds a cover at most twice as large as the
-        optimal cover.
+        optimal cover, with some additional optimizations to reduce the size
+        of the cover.
 
     Args:
         edges: A list of tuples in the form (v1, v2), where v1 and v2 are
@@ -33,11 +34,12 @@ def cover(edges):
     covered = set()
     while len(remaining_edges) > 0:
         current = _find_best_edge(remaining_edges, graph, covered)
-        if _get_vertex_count(graph[current[0]], covered) < 2:
+        start, end = current
+        if _get_vertex_count(graph[start], covered) < 2:
             # Avoid adding vertices with 1 edge, unless both have only one edge
-            cover.add(current[1])
-        elif _get_vertex_count(graph[current[1]], covered) < 2:
-            cover.add(current[0])
+            cover.add(end)
+        elif _get_vertex_count(graph[end], covered) < 2:
+            cover.add(start)
         else:
             cover.update(current)
         # Both should now be covered, regardless of which were added to cover
@@ -47,11 +49,13 @@ def cover(edges):
     return cover
 
 def _get_remaining_edges(edges, covered):
-    """Returns a list of edges where each has two uncovered vertices."""
+    """Returns a list of edges such that each has two uncovered vertices."""
     return [x for x in edges if x[0] not in covered and x[1] not in covered]
 
 def _find_best_edge(edges, graph, covered):
-    """Returns the edge affecting the most highly connected vertices."""
+    """Returns the edge indirectly connected to the most active vertices."""
+    # An edge is considered indirectly connected to a vertex if it connects
+    # to another vertex that has an edge to the first vertex.
     max_edges = 0
     best_edge = edges[0]
     for edge in edges:
@@ -63,6 +67,38 @@ def _find_best_edge(edges, graph, covered):
     return best_edge
 
 def _get_vertex_count(vertex_list, covered):
-    """Returns the number of vertices that are connected to this vertex."""
+    """Returns the number of uncovered vertices in vertex_list."""
     # Filter out vertices already covered
     return sum(1 for v in vertex_list if v not in covered)
+
+def is_valid_cover(edges, cover):
+    """Returns True iff cover is a valid set cover of edges.
+
+    A set cover is a subset of vertices such that for each edge in edges,
+    at least one of the two vertices touching that edge is part of the cover.
+    This function does not check whether the cover is the minimum possible
+    cover, only whether all of the edges are covered by this cover.
+    If cover contains vertex names that are not part of edges, they will not
+    affect the result of this function, since they only serve to make the
+    cover larger.
+
+    Args:
+        edges: A list of tuples in the form (v1, v2), where v1 and v2 are
+            vertex names. v1 and v2 may be strings or numbers.
+
+        cover: A set of vertex names to test for coverage of edges.
+
+    Raises:
+        TypeError: edges does not consist of tuples.
+
+        ValueError: At least one tuple in edges contains fewer than 2 vertices.
+
+    Returns:
+        True if cover is sufficient to cover all edges, or False otherwise.
+        This function always returns True if edges is empty.
+    """
+    for edge in edges:
+        start, end = edge
+        if not (start in cover or end in cover):
+            return False
+    return True
