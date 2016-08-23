30e218ece903e83be1377eccc13f7ee3f4436b94
Mary Ellen Weber <mmweber2@gmail.com>
2016-08-23 11:55:55 -0500

    Add tests for convex hull's get_angle.
    Add tests:
    Angles not Point objects.
    Angles are Points, but are not numbers.
    Both Points are the same.
    Points are 0 degrees apart.
    Points are 45 degrees apart.
    Points are 90 degrees apart.
    Points are 180 degrees apart.
    Points are 270 degrees apart.

--- convex/test_convex.py
+++ convex/test_convex.py
@@ -1,4 +1,5 @@
 from convex_hull import make_hull
+from convex_hull import get_angle
 from collections import namedtuple
 from nose.tools import assert_equals
 from nose.tools import assert_raises
@@ -32,3 +33,35 @@ def test_make_hull_triangle_with_interior():
 def test_make_hull_large():
     result = make_hull([Point(i/10, i % 10) for i in xrange(100)])
     assert_equals(result, set([Point(0,0), Point(9,0), Point(9,9), Point(0,9)]))
+
+def test_get_angle_not_points():
+    assert_raises(AttributeError, get_angle, 1, 2)
+
+def test_get_angle_not_numbers():
+    points = [Point("a", "b"), Point(1, 2)]
+    assert_raises(TypeError, get_angle, *points)
+
+def test_get_angle_same_point():
+    points = [Point(2, 2), Point(2, 2)]
+    assert_equals(get_angle(*points), 0.0)
+
+def test_get_angle_zero_degrees():
+    points = [Point(0, 0), Point(4, 0)]
+    assert_equals(get_angle(*points), 0.0)
+
+def test_get_angle_45_degrees():
+    points = [Point(0, 0), Point(1, 1)]
+    assert_equals(get_angle(*points), 45.0)
+
+def test_get_angle_90_degrees():
+    points = [Point(0, 0), Point(0, 4)]
+    assert_equals(get_angle(*points), 90.0)
+
+def test_get_angle_180_degrees():
+    points = [Point(0, 0), Point(-2, 0)]
+    assert_equals(get_angle(*points), 180.0)
+
+def test_get_angle_270_degrees():
+    points = [Point(0, 0), Point(0, -3)]
+    assert_equals(get_angle(*points), 270.0)
+
