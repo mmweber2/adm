642743809e32a73dd7945c90a8c9400067b791b4
Mary Ellen Weber <mmweber2@gmail.com>
2016-08-25 15:50:15 -0500

    Make bug fixes in bin_pack.

--- bin_packing/bin_packing.py
+++ bin_packing/bin_packing.py
@@ -1,25 +1,50 @@
-def bin_pack(item_list, capacity):
-    """Returns an estimate of the number of boxes needed to pack item_list.
+def bin_pack(item_sizes, capacity):
+    """Returns an estimate of the number of boxes needed to pack item_sizes.
 
     Uses the first-fit decreasing heuristic:
 
-    Sort the boxes by size in decreasing order.
-    For each item, place it in the first box that has room for it.
+    Sort the items by size in decreasing order.
+    For each item in item_sizes, place it in the first box that has room for it.
     If none of the boxes have room, create a new box.
 
     Args:
-        item_list: list of item sizes (numbers), where all items must be packed.
-            All items must have sizes > 0.
+        item_sizes: list of item sizes (numbers) to pack.
+            All sizes must be in the range 0 < size <= capacity.
 
-        capacity: number, the capacity of a single box. Must be > 0.
+        capacity: number, the capacity of a single box.
+            Must be a number >= the largest size in item_sizes.
             All boxes are assumed to have the same capacity.
 
     Returns:
         An integer estimate of the number of boxes required to pack every item
             in item_list into boxes of capacity size, including any partially
             filled boxes.
+            Returns 0 if item_sizes is empty.
 
     Raises:
-        ValueError: capacity is <= 0, or at least one size in item_list is <= 0.
+        ValueError: at least one size in item_sizes is greater than capacity
+            or is less than or equal to 0, or item_sizes contains
+            a non-number item size.
+
+        TypeError: capacity is not a number. 
     """
+    boxes = []
+    sizes = sorted(item_sizes, reverse=True)
+    for size in sizes:
+        if size <= 0:
+            raise ValueError("item size must be > 0")
+        if size > capacity:
+            # Also triggers if size is a string or None
+            raise ValueError("invalid size: {}".format(size))
+        placed_item = False
+        for i in xrange(len(boxes)):
+            if boxes[i] - size >= 0:
+                boxes[i] -= size
+                placed_item = True
+                break
+        if not placed_item:
+            boxes.append(capacity - size)
+    return len(boxes)
+
+
 
