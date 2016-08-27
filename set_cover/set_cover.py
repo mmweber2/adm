b48eddb44a3cb98f3c66dd5e7470c0065c4153df
Mary Ellen Weber <mmweber2@gmail.com>
2016-08-27 09:29:47 -0500

    Write _generate_subsets for exact set cover.

--- set_cover/set_cover.py
+++ set_cover/set_cover.py
@@ -25,3 +25,37 @@ def set_cover(subsets):
         union -= largest
         remaining_subsets.sort(key=lambda x: len(x - covered))
     return cover
+
+def set_cover_exact(subsets):
+    """Returns the smallest set cover for the elements in subsets.
+
+    Generates all combinations of subsets and chooses the smallest subset of
+    subsets that covers the set union.
+    If there are multiple such subsets, returns one arbitrarily.
+
+    Args:
+        subsets: a list of sets. Each set may contain numbers, strings,
+            or other hashable objects.
+
+    Returns:
+        A minimum-size list of sets from subsets that cover the union of all
+            elements in subsets.
+    """
+    # TODO: Write set cover; this just generates all subsets
+    perms = _generate_subsets(subsets)
+    for x in perms:
+        print "Subset: ", x
+
+def _generate_subsets(subsets):
+    all_subsets = []
+    for i in xrange(2**len(subsets)):
+        current = []
+        for j in xrange(len(subsets)):
+            if j > i:
+                break
+            if i & (1 << j):
+                current.append(subsets[j])
+        all_subsets.append(current)
+    return all_subsets
+
+set_cover_exact([set((1, 2, 3)), set((4, 5)), set((2, 3))])
