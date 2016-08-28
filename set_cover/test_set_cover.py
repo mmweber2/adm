5e030c652f666a44077d62fae3af160edf7b1c86
Mary Ellen Weber <mmweber2@gmail.com>
2016-08-28 10:48:56 -0500

    Add four tests for _generate_subsets.

--- set_cover/test_set_cover.py
+++ set_cover/test_set_cover.py
@@ -1,4 +1,5 @@
 from set_cover import set_cover
+from set_cover import _generate_subsets
 from nose.tools import assert_equals
 
 def test_greedy_empty():
@@ -28,3 +29,24 @@ def test_greedy_requires_best_of_four_sets():
     for subset in result:
         assert subset in expected
 
+def test_generate_subsets_empty():
+    assert_equals(_generate_subsets([]), [[set()]])
+
+def test_generate_subsets_single():
+    sets = [set([1, 2])]
+    result = _generate_subsets(sets)
+    assert_equals(len(result), 2)
+    for subset in result:
+        assert subset in [sets, [set()]]
+
+def test_generate_subsets_two():
+    sets = [set([1, 2]), set([2, 3])]
+    expected = [[set([1, 2])], [set([2, 3])], [set()], sets]
+    result = _generate_subsets(sets)
+    assert_equals(len(result), 4)
+    for subset in result:
+        assert subset in expected
+
+def test_generate_subsets_ten():
+    sets = [set(range(i, 10)) for i in xrange(10)]
+    assert_equals(len(_generate_subsets(sets)), 1024)
