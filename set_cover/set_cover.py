9a354ab5e2591b3d6d9be8215098e6735a9c4429
Mary Ellen Weber <mmweber2@gmail.com>
2016-08-29 09:39:17 -0500

    Write set_cover_exact using all subsets.
    Write _is_valid_cover.
    Add error checking to _generate_subsets.

--- set_cover/set_cover.py
+++ set_cover/set_cover.py
@@ -29,9 +29,9 @@ def set_cover(subsets):
 def set_cover_exact(subsets):
     """Returns the smallest set cover for the elements in subsets.
 
-    Generates all combinations of subsets and chooses the smallest subset of
-    subsets that covers the set union.
-    If there are multiple such subsets, returns one arbitrarily.
+    Generates all combinations of subsets and chooses the smallest cover of the
+    set union.
+    If there are multiple such covers, returns one arbitrarily.
 
     Args:
         subsets: a list of sets. Each set may contain numbers, strings,
@@ -39,15 +39,33 @@ def set_cover_exact(subsets):
 
     Returns:
         A minimum-size list of sets from subsets that cover the union of all
-            elements in subsets.
+            elements in subsets. Returns an empty list if subsets is empty.
     """
-    # TODO: Write set cover; this just generates all subsets
-    perms = _generate_subsets(subsets)
+    if len(subsets) == 0:
+        return []
+    for i in xrange(1, 2**len(subsets)):
+        # Start with covers of size 1 and increases size by 1 each time,
+        # so the first cover found will be the smallest possible cover
+        for cover in _generate_subsets(subsets, i):
+            if _is_valid_cover(cover):
+                return cover
+
+def _is_valid_cover(all_subsets, cover):
+    """Returns True iff cover is a valid set cover for all_subsets."""
+    # Put all items of all_subsets or cover into sets
+    covered = set(item for subset in cover for item in subset)
+    union = set(item for subset in all_subsets for item in subset)
+    return covered == union
 
 def _generate_subsets(subsets, max_size=None):
     """Given a set of subsets, generate all combinations of subsets."""
+    if max_size == 0:
+        return []
     if max_size is None:
         max_size = len(subsets)
+    if max_size < 0:
+        raise ValueError("max_size cannot be negative")
+    # Empty set is included in all subsets of size 1 or greater
     all_subsets = [[set()]]
     for i in xrange(2**len(subsets)):
         current = []
