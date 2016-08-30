f60c3a8d482ddaab0938ced7de07a88d029f272e
Mary Ellen Weber <mmweber2@gmail.com>
2016-08-30 15:44:18 -0500

    Update tests to fit changes in set_cover_exact.

--- set_cover/test_set_cover.py
+++ set_cover/test_set_cover.py
@@ -38,42 +38,23 @@ def test_greedy_large():
     assert _is_valid_cover(sets, result)
 
 def test_generate_subsets_empty():
-    assert_equals(_generate_subsets([]), [[set()]])
+    assert_equals(_generate_subsets([]), [])
 
 def test_generate_subsets_single():
     sets = [set([1, 2])]
     result = _generate_subsets(sets)
-    expected = [sets, [set()]]
-    assert_equals(len(result), 2)
-    for subset in result:
-        assert subset in expected
-    for subset in expected:
-        assert subset in result
+    assert_equals([tuple(sets)], result)
 
 def test_generate_subsets_two():
     sets = [set([1, 2]), set([2, 3])]
-    expected = [[set([1, 2])], [set([2, 3])], [set()], sets]
     result = _generate_subsets(sets)
-    assert_equals(len(result), len(expected))
-    for subset in result:
-        assert subset in expected
-    for subset in expected:
-        assert subset in result
-
-def test_generate_subsets_four():
-    sets = [set([1, 3]), set([2, 3]), set([1, 3, 4]), set([1, 4])]
-    result = _generate_subsets(sets)
-    assert_equals(len(result), 16)
-
-def test_generate_subsets_ten():
-    sets = [set(range(i, 10)) for i in xrange(10)]
-    assert_equals(len(_generate_subsets(sets)), 1024)
+    assert_equals([tuple(sets)], result)
 
 def test_generate_subsets_size_one():
     sets = [set([1, 2]), set([2, 3]), set([3, 4])]
-    expected = [[set([1, 2])], [set([2, 3])], [set([3, 4])], [set()]]
+    expected = [(set([1, 2]),), (set([2, 3]),), (set([3, 4]),)]
     result = _generate_subsets(sets, 1)
-    assert_equals(len(result), 4)
+    assert_equals(len(result), len(expected))
     for subset in result:
         assert subset in expected
     for subset in expected:
@@ -81,9 +62,8 @@ def test_generate_subsets_size_one():
 
 def test_generate_subsets_size_two():
     sets = [set([1, 2]), set([2, 3]), set([3, 4])]
-    expected = [[set([1, 2])], [set([2, 3])], [set([3, 4])], [set()], 
-            [set([1, 2]), set([2, 3])], [set([1, 2]), set([3, 4])],
-            [set([2, 3]), set([3, 4])]]
+    expected = [(set([1, 2]), set([2, 3])), (set([1, 2]), set([3, 4])),
+            (set([2, 3]), set([3, 4]))]
     result = _generate_subsets(sets, 2)
     assert_equals(len(result), len(expected))
     for subset in result:
@@ -127,11 +107,11 @@ def test_exact_empty():
 
 def test_exact_single_set():
     sets = [set([0, 2])]
-    assert_equals(set_cover_exact(sets), sets)
+    assert_equals(set_cover_exact(sets), tuple(sets))
 
 def test_exact_best_of_two_sets():
     sets = [set([0, 3]), set([3])]
-    assert_equals(set_cover_exact(sets), [sets[0]])
+    assert_equals(set_cover_exact(sets), (sets[0], ))
 
 def test_exact_requires_two_sets():
     sets = [set([1, 3]), set([2, 3])]
@@ -150,5 +130,5 @@ def test_exact_best_of_four_sets():
 
 def test_exact_large():
     sets = [set(range(i, 10)) for i in xrange(10)]
-    #result = set_cover_exact(sets)
-    #assert _is_valid_cover(sets, result)
+    result = set_cover_exact(sets)
+    assert _is_valid_cover(sets, result)
