43721ef37e31663916bd348e7b9b482c5a193033
Mary Ellen Weber <mmweber2@gmail.com>
2016-08-30 14:52:01 -0500

    Update bin_packing comments per code review.

--- bin_packing/bin_packing.py
+++ bin_packing/bin_packing.py
@@ -2,24 +2,17 @@ def bin_pack(item_sizes, capacity):
     """Returns an estimate of the number of boxes needed to pack item_sizes.
 
     Uses the first-fit decreasing heuristic:
-
-    Sort the items by size in decreasing order.
-    For each item in item_sizes, place it in the first box that has room for it.
-    If none of the boxes have room, create a new box.
+        Sort the items by size in decreasing order.
+        For each item, place it in the first box that has room for it.
+        If none of the boxes have room, create a new box.
 
     Args:
-        item_sizes: list of item sizes (numbers) to pack.
+        item_sizes: list of item sizes (integers or floats) to pack.
             All sizes must be in the range 0 < size <= capacity.
 
-        capacity: number, the capacity of a single box.
+        capacity: integer or float, the capacity of a single box.
             Must be a number >= the largest size in item_sizes.
             All boxes are assumed to have the same capacity.
-
-    Returns:
-        An integer estimate of the number of boxes required to pack every item
-            in item_list into boxes of capacity size, including any partially
-            filled boxes.
-            Returns 0 if item_sizes is empty.
 
     Raises:
         ValueError: at least one size in item_sizes is greater than capacity
@@ -27,6 +20,12 @@ def bin_pack(item_sizes, capacity):
             a non-number item size.
 
         TypeError: capacity is not a number. 
+
+    Returns:
+        An integer estimate of the number of boxes required to pack every item
+            in item_list into boxes of capacity size, including any partially
+            filled boxes.
+            Returns 0 if item_sizes is empty.
     """
     boxes = []
     sizes = sorted(item_sizes, reverse=True)
@@ -39,10 +38,12 @@ def bin_pack(item_sizes, capacity):
         placed_item = False
         for i in xrange(len(boxes)):
             if boxes[i] - size >= 0:
+                # Item fits in an existing box
                 boxes[i] -= size
                 placed_item = True
                 break
         if not placed_item:
+            # Make a new box
             boxes.append(capacity - size)
     return len(boxes)
 
