6e367d5313c8d30102768dbfd32e741b68492616
Mary Ellen Weber <mmweber2@gmail.com>
2016-09-02 09:24:02 -0500

    Continue working on string_match.
    Remove _match_substrings function since it is not used.

--- string_matching/string_match.py
+++ string_matching/string_match.py
@@ -23,32 +23,31 @@ def string_match(text, pattern):
     """
     bad_chars = _bad_character_table(pattern)
     general_table, special_table = _good_suffix_tables(pattern)
-    # Indices where a match of pattern begins in string
+    # Indices where a match of pattern begins in text
     matches = []
     start_pos = 0
-    # Line up pattern at i, so end of pattern lies at pos
-    end_pos = start_pos + len(pattern) - 1
+    # Line up pattern at i, so end of pattern lies at end_pos in text
+    end_pos = len(pattern) - 1
     while end_pos < len(text):
         pattern_pos = len(pattern) - 1
         # Attempt to match pattern to string
         while pattern_pos >= 0 and text[end_pos] == pattern[pattern_pos]:
             pattern_pos -= 1
             end_pos -= 1
-        # Found a full match of pattern
         if pattern_pos == -1:
-            matches.append(end_pos - len(pattern) + 1)
+            # Found a full match of pattern
+            matches.append(start_pos)
             if len(pattern) > 1:
                 end_pos += len(pattern) - special_table[1]
-            else:
-                end_pos += 1
         else:
             # Bad character rule result
             text_char = text[end_pos]
-            print "Text char is {} and pattern_char is {}".format(text_char, pattern[pattern_pos])
-            print "Bad chars {} is ".format(text_char), bad_chars[text_char]
-            bc_result = pattern_pos - bad_chars[text_char][pattern_pos]
+            if pattern_pos not in bad_chars[text_char]:
+                bc_result = -1
+            else:
+                bc_result = pattern_pos - bad_chars[text_char][pattern_pos]
             if pattern_pos + 1 == len(pattern):
-                # Mismatch at end of pattern
+                # Mismatch at first character checked
                 suffix_result = 0
             elif general_table[pattern_pos + 1] == -1:
                 # Suffix is not part of pattern
@@ -62,11 +61,7 @@ def string_match(text, pattern):
         end_pos = start_pos + len(pattern) - 1
     return matches
 
-# When characters don't match, good suffix will always be 0, so we will take the result from bad character
-
-# TODO: Should bad char return -1 or 0?
 def _bad_character_table(pattern):
-    # TODO: Make result -1 for all chars by default
     """Creates a 'bad character rule' table for Boyer-Moore string searches."""
     table = defaultdict(dict)
     # Mapping: [Char][index in pattern]: first index of character in pattern 
@@ -74,6 +69,9 @@ def _bad_character_table(pattern):
         # Set lookup to the highest index below i that matches the character,
         # or -1 if it doesn't match (which is also rfind's default behavior)
         table[c][i] = pattern.rfind(c, 0, i)
+        # We'd like to return -1 for any character not in the table, but
+        # even if we initialize that for alphabet characters, we'd still get
+        # errors when checking for spaces, punctuation, etc.
     return table
     
 def _good_suffix_tables(pattern):
@@ -94,16 +92,3 @@ def _good_suffix_tables(pattern):
                 special_table[i] = 1 - j - i
                 break
     return general_table, special_table
-
-def _match_substrings(s, index1, index2):
-    """Returns the length of the matching substring from indices 1 and 2."""
-    if index1 == index2:
-        # Entire rest of string matches
-        return len(s) - index1
-    count = 0
-    if index2 < index1:
-        index1, index2 = index2, index1
-    while index2 < len(s) and s[index1] == s[index2]:
-        count += 1
-    return count
-
